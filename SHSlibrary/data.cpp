230b3ea0c5ced4f26abf991bfeb98be3160d0b22
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-10-10 00:18:58 +0300

    Убраны старые наброски, доработан класс Byte Collector

---
+++ SHSlibrary/data.cpp
@@ -0,0 +1,118 @@
+#pragma once
+#include "data.h"
+
+// SHSDataTransfer::SHSDataTransfer(Stream* stream, uint8_t ID) {
+//     _ID = ID;
+//     _stream = stream;
+// }
+
+// template
+// void SHSDataTransfer::tick(T data) {
+//     if (stream.abailable()) {
+//         uint8_t* ptr = (uint8_t*)&data;
+//         while (stream.available()) {
+//             *ptr++ = stream.read();
+//         }
+//     }
+// }
+
+// void SHSDataTransfer::attach(void(*attached)) {
+//     _attached = *attached;
+// }
+
+
+template <typename T> 
+void ByteCollector::add(T& value) {
+    packBytes(ptr, value);
+    ptr += sizeof(T);
+}
+
+
+template <typename T>
+void ByteCollector::add(T& value, uint8_t bytes) {
+    uint8_t* _ptr = &(value);
+    _ptr += sizeof(T) - bytes;
+
+    for (uint8_t i = 0; i < bytes; i++) ptr++ = _ptr++;
+
+}
+
+
+template <typename T>
+void ByteCollector::addVal(T value) {
+    packBytes(ptr, value);
+    ptr += sizeof(T);
+}
+
+
+template <typename T>
+void ByteCollector::addVal(T value, uint8_t bytes) {
+    uint8_t* _ptr = &(value);
+    _ptr += sizeof(T) - bytes;
+
+    for (uint8_t i = 0; i < bytes; i++) ptr++ = _ptr++;
+
+}
+
+
+
+uint16_t ByteCollector::size() {
+    return static_cast<uint16_t>(ptr - buf);
+}
+
+
+
+
+// CRC
+template <typename T>
+uint8_t getCRC(T& data) {
+    uint8_t crc = 0;
+    uint8_t* ptr = (uint8_t*)&data;
+
+    for (uint16_t i = 0; i < sizeof(T); i++) crc_update(crc, *ptr);
+    return crc;
+}
+
+
+void crc_update(uint8_t& crc, uint8_t data) {
+#if defined (__AVR__)
+    // резкий алгоритм для AVR
+    uint8_t counter;
+    uint8_t buffer;
+    asm volatile (
+        "EOR %[crc_out], %[data_in] \n\t"
+        "LDI %[counter], 8          \n\t"
+        "LDI %[buffer], 0x8C        \n\t"
+        "_loop_start_%=:            \n\t"
+        "LSR %[crc_out]             \n\t"
+        "BRCC _loop_end_%=          \n\t"
+        "EOR %[crc_out], %[buffer]  \n\t"
+        "_loop_end_%=:              \n\t"
+        "DEC %[counter]             \n\t"
+        "BRNE _loop_start_%="
+        : [crc_out] "=r" (crc), [counter]"=d" (counter), [buffer]"=d" (buffer)
+        : [crc_in] "0" (crc), [data_in]"r" (data)
+        );
+#else
+    // обычный для всех остальных
+    uint8_t i = 8;
+    while (i--) {
+        crc = ((crc ^ data) & 1) ? (crc >> 1) ^ 0x8C : (crc >> 1);
+        data >>= 1;
+    }
+#endif
+}
+
+
+uint8_t crc_bytes(uint8_t* buffer, uint8_t size) {
+    uint8_t crc = 0;
+    for (uint8_t i = 0; i < size; i++) crc_update(crc, buffer[i]);
+    return crc;
+}
+
+
+float getAnalogAverage(uint8_t pin, uint8_t samples = 20) {
+    uint16_t aver = 0;
+    for (uint8_t i = 0; i < samples; i++) aver += analogRead(pin);
+    return (float)aver / samples;
+}

1b0554dec87795f0ee6e6cedd5255ae1baa1d670
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-10-10 22:33:53 +0300

    ByteCollector переехал в отдельный файлик. Код максимально доработан и оптимизирован, исправлены все ошибки компиляции.

---
+++ SHSlibrary/ByteCollector.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+class ByteCollector {
+public:
+    uint8_t* buf;
+    uint8_t* ptr;
+
+    ByteCollector(uint8_t size): buf(new uint8_t[size]) {
+
+    }
+
+
+
+    uint16_t size() {
+        return static_cast<uint16_t>(ptr - buf);
+    }
+
+
+    template <typename T>
+    void add(T& value, uint8_t bytes = sizeof(T)) {
+        uint8_t* _ptr = (uint8_t*)&(value);
+        _ptr += sizeof(T) - bytes;
+
+        for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
+
+    }
+
+
+    template <typename T>
+    void addVal(T value, uint8_t bytes = sizeof(T)) {
+        uint8_t* _ptr = (uint8_t*)&(value);
+        _ptr += sizeof(T) - bytes;
+
+        for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
+
+    }
+};

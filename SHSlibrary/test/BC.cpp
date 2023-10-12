34a3cedd5a11a1e14f7f3bc151bd76fde525a950
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-10-12 09:43:20 +0300

    ByteCollector разделен на файл реализации

---
+++ SHSlibrary/test/BC.cpp
@@ -0,0 +1,36 @@
+#include "BC.h"
+
+
+ByteCollector::ByteCollector(uint8_t size): buf(new uint8_t[size]) {
+    ptr = buf;
+}
+
+ByteCollector::~ByteCollector() {
+
+    delete[] buf;
+
+}
+
+uint16_t ByteCollector::size() {
+    return static_cast<uint16_t>(ptr - buf);
+}
+
+
+template <typename T>
+void ByteCollector::add(T& value, uint8_t bytes = sizeof(T)) {
+    uint8_t* _ptr = (uint8_t*)&(value);
+    _ptr += sizeof(T) - bytes;
+
+    for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
+
+}
+
+
+template <typename T>
+void ByteCollector::addVal(T value, uint8_t bytes = sizeof(T)) {
+    uint8_t* _ptr = (uint8_t*)&(value);
+    _ptr += sizeof(T) - bytes;
+
+    for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
+
+}

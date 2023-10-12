0edbc4ade30291c7fb337179a3378d6f60abc4a0
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-10-12 09:43:57 +0300

    Проверяет ByteCollector на чистом C++

---
+++ SHSlibrary/test/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define LIB 0
+
+#if (LIB == 0)
+#include "..\ByteCollector.h"
+#else
+#include "BC.h"
+#endif
+
+int main() {
+  
+    ByteCollector c(32);
+    
+    uint8_t val1 = 25;
+    uint16_t val2 = 1028;
+    int64_t val3 = INT64_MAX;
+    
+    c.add(val1);
+    c.add(val2);
+    c.add(val3);
+    c.addVal(34);
+
+    std::cout << static_cast<int>(c.buf[0]) << ' ';
+    std::cout << static_cast<int>(c.buf[1 + 2 + 8]) << std::endl;
+
+    
+    return 0;
+}

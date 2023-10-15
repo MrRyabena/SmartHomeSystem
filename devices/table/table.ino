44a6aa7cf993239a1a0b6fd960f1b0a6085753f0
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-10-15 19:47:08 +0300

    Набросано, работает!

--- devices/table/table.ino
+++ devices/table/table.ino
@@ -5,6 +5,7 @@
 
 #include <GBUS.h>
 GBUS bus(&Serial, TABLE_ID, 25);
+DTP dtp(&bus, TABLE_ID, BUSbufsize);
 
 #include <EncButton.h>
 Button btn(BTNpin, INPUT, HIGH);
@@ -12,17 +13,21 @@ Button btn(BTNpin, INPUT, HIGH);
 #include <GyverNTP.h>
 GyverNTP ntp(3);
 
-BotDebbuger bot{};
+#include <FastBot.h>
+FastBot bot(BOT_TOKEN);
 
 
 void setup() {
   Serial.begin(BUSspeed);
   connectWiFi();
-  bot.send("I am started");
+  botSetup();
+
+  bot.sendMessage("I am started", CHAT_ID);
 }
 
 void loop() {
   //ntp.tick();
+  bot.tick();
+
   handleBus();
-  handleButton();
 }

e609a2279dc6bc927919f3c273a0834b2bc57bb4
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-04 23:27:53 +0300

    Доработан и переписан

--- SHSlibrary/ByteCollector.h
+++ SHSlibrary/ByteCollector.h
@@ -1,43 +1,53 @@
 #pragma once
 
-class ByteCollector {
+class ByteCollector
+{
 public:
-    uint8_t* buf{};
-    uint8_t* ptr{};
+    uint8_t *buf{};
+    uint8_t *ptr{};
 
-    ByteCollector(uint8_t size): buf(new uint8_t[size]) {
+    ByteCollector(uint8_t size) : buf(new uint8_t[size])
+    {
         ptr = buf;
+        _size = size;
     }
 
-    ~ByteCollector() {
+    ~ByteCollector()
+    {
         delete[] buf;
-        // buf = nullptr;
-        // ptr = nullptr;
     }
 
-
-
-    uint16_t size() {
+    uint16_t size()
+    {
         return static_cast<uint16_t>(ptr - buf);
     }
 
-
     template <typename T>
-    void add(const T& value, uint8_t bytes = sizeof(T)) {
-        uint8_t* _ptr = (uint8_t*)&(value);
-        //_ptr += sizeof(T) - bytes;
+    void add(const T &value, uint8_t bytes = sizeof(T))
+    {
+        if (_size - (ptr - buf) < bytes)
+            reserve(bytes - (_size - (ptr - buf)));
 
-        for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
-
+        uint8_t *_ptr = (uint8_t *)&(value);
+        for (uint8_t i = 0; i < bytes; i++)
+            *ptr++ = *_ptr++;
     }
 
+    void reserve(uint8_t size)
+    {
+        if (!size)
+            return;
 
-    template <typename T>
-    void addVal(const T value, uint8_t bytes = sizeof(T)) {
-        uint8_t* _ptr = (uint8_t*)&(value);
-        //_ptr += sizeof(T) - bytes;
+        _size += size;
+        uint8_t *n_buf = new uint8_t[_size];
+        for (uint16_t i = 0; i < (ptr - buf); i++)
+            n_buf[i] = buf[i];
 
-        for (uint8_t i = 0; i < bytes; i++) *ptr++ = *_ptr++;
-
+        ptr = n_buf + (ptr - buf);
+        delete[] buf;
+        buf = n_buf;
     }
+
+private:
+    uint16_t _size{};
 };

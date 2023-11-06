c64a86f851051fd5142217f90ff4b4413affda00
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-07 01:22:26 +0300

    Добавлено и протестировано расширение вперед

--- SHSlibrary/ByteCollector.h
+++ SHSlibrary/ByteCollector.h
@@ -35,6 +35,15 @@ namespace shs
                 *ptr++ = *_ptr++;
         }
 
+
+        template <typename T>
+        void addBefore(const T &value, uint8_t bytes = sizeof(T)) {
+            if (!_beforeSize) reserveBefore(bytes);
+            if (_beforeSize - (_ptrBefore - buf) < bytes) reserveBefore(bytes);
+            uint8_t *_ptr = (uint8_t*)&value;
+            for (uint8_t i = 0; i < bytes; i++) *_ptrBefore++ = *_ptr++;
+        }
+
         void reserve(uint8_t size)
         {
             if (!size)
@@ -46,11 +55,28 @@ namespace shs
                 n_buf[i] = buf[i];
 
             ptr = n_buf + (ptr - buf);
+            _ptrBefore = n_buf + (_ptrBefore - buf);
             delete[] buf;
             buf = n_buf;
         }
 
+
+        void reserveBefore(uint8_t size) {
+            if(!size) return;
+            _size += size;
+            _beforeSize = size;
+            uint8_t* n_buf = new uint8_t[_size];
+            for (uint16_t i = size; i < (ptr - buf); i++) n_buf[i] = buf[i - size];
+
+            ptr = n_buf + size + (ptr - buf);
+            delete [] buf;
+            buf = n_buf;
+            _ptrBefore = buf;
+        }
+
     private:
         uint16_t _size{};
+        uint16_t _beforeSize{};
+        uint8_t  *_ptrBefore{};
     };
 };
--- SHSlibrary/test/VS/testBC/main.cpp
+++ SHSlibrary/test/VS/testBC/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define LIB 1
+#define LIB 0
 
 #if (LIB == 0)
 #include "..\..\..\ByteCollector.h"
@@ -7,9 +7,10 @@
 #include "..\..\BC.h"
 #endif
 
-int main() {
+int main()
+{
 
-    ByteCollector c(32);
+    shs::ByteCollector c(32);
 
     uint8_t val1 = 25;
     uint16_t val2 = 1028;
@@ -18,11 +19,25 @@ int main() {
     c.add(val1);
     c.add(val2);
     c.add(val3);
-    c.addVal(34);
+    c.add(34);
 
     std::cout << static_cast<int>(c.buf[0]) << ' ';
     std::cout << static_cast<int>(c.buf[1 + 2 + 8]) << std::endl;
+    std::cout << c.size() << std::endl;
 
+    c.reserveBefore(3);
+    std::cout << c.size() << std::endl;
+    // c.buf[0] = 2;
+    c.addBefore(2, 1);
+    c.addBefore(1111, 2);
+    c.addBefore(4444, 2);
+    std::cout << c.size() << std::endl;
+
+    std::cout << (((uint16_t)c.buf[1] << 8) + c.buf[0]) << ' ';
+    std::cout << static_cast<int>(c.buf[5]) << ' ';
+    std::cout << (((uint16_t)c.buf[4] << 8) + c.buf[3]) << std::endl;
+    std::cout << static_cast<int>(c.buf[0 + 5]) << ' ';
+    std::cout << static_cast<int>(c.buf[1 + 2 + 8 + 5]) << std::endl;
 
     return 0;
 }

810fcb6b247c42142b83145045a1bd46b594a161
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-06 13:14:56 +0300

    Обернул в namespace

--- SHSlibrary/ByteCollector.h
+++ SHSlibrary/ByteCollector.h
@@ -1,53 +1,56 @@
 #pragma once
 
-class ByteCollector
+namespace shs
 {
-public:
-    uint8_t *buf{};
-    uint8_t *ptr{};
-
-    ByteCollector(uint8_t size) : buf(new uint8_t[size])
+    class ByteCollector
     {
-        ptr = buf;
-        _size = size;
-    }
+    public:
+        uint8_t *buf{};
+        uint8_t *ptr{};
 
-    ~ByteCollector()
-    {
-        delete[] buf;
-    }
+        ByteCollector(uint8_t size) : buf(new uint8_t[size])
+        {
+            ptr = buf;
+            _size = size;
+        }
 
-    uint16_t size()
-    {
-        return static_cast<uint16_t>(ptr - buf);
-    }
+        ~ByteCollector()
+        {
+            delete[] buf;
+        }
 
-    template <typename T>
-    void add(const T &value, uint8_t bytes = sizeof(T))
-    {
-        if (_size - (ptr - buf) < bytes)
-            reserve(bytes - (_size - (ptr - buf)));
+        uint16_t size()
+        {
+            return static_cast<uint16_t>(ptr - buf);
+        }
 
-        uint8_t *_ptr = (uint8_t *)&(value);
-        for (uint8_t i = 0; i < bytes; i++)
-            *ptr++ = *_ptr++;
-    }
+        template <typename T>
+        void add(const T &value, uint8_t bytes = sizeof(T))
+        {
+            if (_size - (ptr - buf) < bytes)
+                reserve(bytes - (_size - (ptr - buf)));
 
-    void reserve(uint8_t size)
-    {
-        if (!size)
-            return;
+            uint8_t *_ptr = (uint8_t *)&(value);
+            for (uint8_t i = 0; i < bytes; i++)
+                *ptr++ = *_ptr++;
+        }
 
-        _size += size;
-        uint8_t *n_buf = new uint8_t[_size];
-        for (uint16_t i = 0; i < (ptr - buf); i++)
-            n_buf[i] = buf[i];
+        void reserve(uint8_t size)
+        {
+            if (!size)
+                return;
 
-        ptr = n_buf + (ptr - buf);
-        delete[] buf;
-        buf = n_buf;
-    }
+            _size += size;
+            uint8_t *n_buf = new uint8_t[_size];
+            for (uint16_t i = 0; i < (ptr - buf); i++)
+                n_buf[i] = buf[i];
 
-private:
-    uint16_t _size{};
+            ptr = n_buf + (ptr - buf);
+            delete[] buf;
+            buf = n_buf;
+        }
+
+    private:
+        uint16_t _size{};
+    };
 };

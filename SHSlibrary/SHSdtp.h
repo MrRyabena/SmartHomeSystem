cb38edba1c296f99afb1caf79f1fbbb88692c30e
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-14 02:00:53 +0300

    неокторые доработки

--- SHSlibrary/SHSdtp.h
+++ SHSlibrary/SHSdtp.h
@@ -3,45 +3,95 @@
 */
 
 #pragma once
-
+#include "SHSalgoritm.h"
 #define DTP_OFFSETbeg 3
 
-class DTP
+namespace shs
 {
-
-public:
-    DTP(uint8_t ID)
+    class DTP
     {
-        _myID = ID;
+
+    public:
+        DTP(uint8_t ID)
+        {
+            _myID = ID;
+        };
+
+        ~DTP()
+        {
+        }
+
+        // template <typename T>
+        // uint8_t packDTPdata(uint8_t* buffer, uint8_t bufSize, T& data, uint8_t to, uint8_t from) {
+        //     buffer[0] = sizeof(T) + DTP_OFFSET;      // размер пакета с учётом служебных
+        //     if (buffer[0] > bufSize) return 0;        // если переполним буфер
+        //     buffer[1] = to;                           // адрес приёмника
+        //     buffer[2] = from;                        // адрес передатчика
+        //     const uint8_t* ptr = (const uint8_t*)&data;                                // указатель
+        //     for (uint16_t i = 0; i < sizeof(T); i++) buffer[i + 3] = *ptr++;            // пакуем дату
+        //     if (GBUS_CRC) buffer[sizeof(T) + 3] = GBUS_crc_bytes(buffer, sizeof(T) + 3);// crc
+        //     return buffer[0];
+        // }
+
+        uint8_t packDTP(shs::ByteCollector *bc, uint8_t to)
+        {
+            packDTP(bc, to, _myID);
+        }
+
+        uint8_t packDTP(shs::ByteCollector *bc, uint8_t to, uint8_t from)
+        {
+            bc->reserveBefore(DTP_OFFSETbeg);
+            bc->buf[1] = to;
+            bc->buf[2] = from;
+            bc->add(shs::crc_16(bc->buf, bc->size()));
+            bc->buf[0] = bc->size();
+        }
+
+        uint8_t checkDTP(shs::ByteCollector *bc)
+        {
+            if (bc->size() < bc->buf[0])
+                return 1;
+
+            uint16_t crc = shs::crc_16(bc->buf, bc->size());
+            if (bc->buf[bc->buf[0] - 1] != crc)
+                return 2;
+            return 0;
+        }
+
+        uint8_t parseDTP(shs::ByteCollector *bc, void (*callback)(shs::DTPdata&, shs::ByteCollector*))
+        {
+            uint8_t status = checkDTP(bc);
+            if (status)
+                return status;
+
+            shs::DTPdata data{};
+            data.from = bc->buf[2];
+            data.to = bc->buf[1];
+            data.datasize = bc->size() - DTP_OFFSETbeg - 1;
+            bc->readPtr = buf + DTP_OFFSETbeg;
+            callback(data, bc);
+            return 0;
+        }
+
+    private:
+        uint8_t _myID{};
     };
-
-    ~DTP()
-    {
-    }
-
-
-    // template <typename T>
-    // uint8_t packDTPdata(uint8_t* buffer, uint8_t bufSize, T& data, uint8_t to, uint8_t from) {
-    //     buffer[0] = sizeof(T) + DTP_OFFSET;      // размер пакета с учётом служебных
-    //     if (buffer[0] > bufSize) return 0;        // если переполним буфер
-    //     buffer[1] = to;                           // адрес приёмника
-    //     buffer[2] = from;                        // адрес передатчика
-    //     const uint8_t* ptr = (const uint8_t*)&data;                                // указатель
-    //     for (uint16_t i = 0; i < sizeof(T); i++) buffer[i + 3] = *ptr++;            // пакуем дату
-    //     if (GBUS_CRC) buffer[sizeof(T) + 3] = GBUS_crc_bytes(buffer, sizeof(T) + 3);// crc
-    //     return buffer[0];
-    // }
-
-    uint8_t packDTP(shs::ByteCollector *bc, uint8_t to, uint8_t from = _myID)
-    {
-        bc->reserveBefore(DTP_OFFSET);
-        bc->buf[1] = to;
-        bc->buf[2] = from;
-        bc->add(shs::crc_16(bc->buf, bc->size()));
-        bc->buf[0] = bc->size();
-    }
- 
-
-private:
-    uint8_t _myID = 0;
 };
+
+namespace shs
+{
+    struct DTPdata
+    {
+        uint8_t from{};
+        uint8_t to{};
+        uint8_t datasize{};
+    }
+}
+
+
+namespace shs {
+    enum DTPcommands : uint8_t {
+        answer = 253,
+        request,
+    }
+}
--- SHSlibrary/SHSlibrary.h
+++ SHSlibrary/SHSlibrary.h
@@ -1,12 +1,12 @@
 #include "settings.h"
-#ifndef __AVR__
-#include "connectWiFi.h"
-#include "OTAupdate.h"
-//#include "data.h"
-#include <development/bot_debbuger.h>
-#endif
-#include "SHSalgoritm.h"
-#include "ByteCollector.h"
-#include "SHSdtp.h"
-#include "SHSfs.h"
+// #ifndef __AVR__
+// #include "connectWiFi.h"
+// #include "OTAupdate.h"
+// //#include "data.h"
+// #include <development/bot_debbuger.h>
+// #endif
+// #include "SHSalgoritm.h"
+// #include "ByteCollector.h"
+// #include "SHSdtp.h"
+// #include "SHSfs.h"
 

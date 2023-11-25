8d723f1252da9fabc71d144ba970792838121cfe
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-25 17:14:12 +0300

    Добавлено копирование новой папки

--- synchronizer/synchronizer.cpp
+++ synchronizer/synchronizer.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <windows.h>
-#include<direct.h>
-
+#include <direct.h>
 
 using namespace std;
 
-
 #define LIB "C:/Users/Daniil/Documents/Arduino/libraries/SHS_library"
 #define INP "B:/SmartHomeSystem/SHSlibrary"
+#define LIB2 "C:/Users/Daniil/Documents/Arduino/libraries/devAPI"
+#define INP2 "B:/SmartHomeSystem/devAPI"
 
-
-
-int main() {
-
-
+int main()
+{
 
   system("RD /S /Q \"" LIB "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
   mkdir(LIB);
-  
   system("xcopy \"" INP "\" \"" LIB "\" /e");
 
-  int a;
-  cin >> a;
-}
+  system("RD /S /Q \"" LIB2 "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
+  mkdir(LIB2);
+  system("xcopy \"" INP2 "\" \"" LIB2 "\" /e");
 
+  system("pause");
+
+  return 0;
+}

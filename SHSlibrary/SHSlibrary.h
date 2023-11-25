ffe71f1e16f78c9d7a8202775495504d36639fc3
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2023-11-25 17:14:46 +0300

    Сделано корректно

--- SHSlibrary/SHSlibrary.h
+++ SHSlibrary/SHSlibrary.h
@@ -1,12 +1,15 @@
-#include "settings.h"
-// #ifndef __AVR__
-// #include "connectWiFi.h"
-// #include "OTAupdate.h"
-// //#include "data.h"
-// #include <development/bot_debbuger.h>
-// #endif
-// #include "SHSalgoritm.h"
-// #include "ByteCollector.h"
-// #include "SHSdtp.h"
-// #include "SHSfs.h"
+#pragma once
 
+#include "SHSsettings.h"
+
+#ifndef __AVR__
+#include "SHSconnectWiFi.h"
+//#include "OTAupdate.h"
+#include "SHSfs.h"
+// #include "data.h"
+//  #include <development/bot_debbuger.h>
+#endif
+
+#include "SHSByteCollector.h"
+#include "SHSdtp.h"
+#include "SHSalgoritm.h"

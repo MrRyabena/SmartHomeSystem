fa9f1dd1075ab8a36497c925d3ad24dd3b5258f6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-01-12 17:19:37 +0300

    Придумал как упаковать все в класс

--- SHSlibrary/SHSupdate.h
+++ SHSlibrary/SHSupdate.h
@@ -1,41 +1,23 @@
 #pragma once
-
+#include "SHSsettings.h"
 #include <FastBot.h>
 
 namespace shs
 {
-    void updateRun();
-    FastBot *_update_bot{};
-    void _update_newMes(FB_msg &message);
+    class Update;
 };
 
-void shs::updateRun()
+class shs::Update
 {
-    _update_bot = new FastBot(UPBOT_TOKEN);
-    shs::_update_bot->attach(shs::_update_newMes);
-    uint32_t tmr = millis();
-    for (;;)
-    {
-        shs::_update_bot->tick();
-        yield();
-        if (millis() - tmr >= 60000)
-            break;
-    }
-    _update_bot->sendmessage("")
-}
+public:
+    Update();
+    ~Update();
 
-void shs::_update_newMes(FB_msg &message)
-{
-    if (message.OTA && message.text.toInt() == shs::_update_ID)
-    {
-        if (message.fileName.indexOf("mklittlefs") > 0 || message.fileName.indexOf("spiffs") > 0)
-        {
-            shs::_update_bot->updateFS(); // update spiffs
-        }
-        else
-        {
-            shs::_update_bot->update(); // update sketch
-        }
-    } 
-    _update_bot->sendmessage("Error! Invalid ID", message.chatID);
-}
+    void start(const uint32_t timeout = 120000, const bool resturt = true);
+    void setHandler(void (*handler)(FB_msg &message));
+
+private:
+    void _newMes(FB_msg &message);
+    void (*_handler)(FB_msg &message, FastBot &upbot) = nullptr;
+};
+

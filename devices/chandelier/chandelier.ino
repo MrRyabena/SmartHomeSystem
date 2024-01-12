0178be9277b82e56b44a9d5beae9f2f58c47a2c4
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-01-12 17:18:08 +0300

    Почти все дописано, сделано с новыми инструментами, не тестировалось и не компилировалось

--- devices/chandelier/chandelier.ino
+++ devices/chandelier/chandelier.ino
@@ -1,27 +1,44 @@
 #include <SHSlibrary.h>
 #include <GyverDimmer.h>
+#include <SHSmodule.h>
 
 #define Dpin 12
 
 DimmerBres<Dpin> dim;
 IRAM_ATTR void ist();
 
+shs::Processes processes(dtpHandler);
+
+void dtpHandler(shs::DTPdata &d);
+
 void setup()
 {
-    shs::connectWiFi();
-    
+    processes.setup();
+
     pinMode(14, INPUT);
     attachInterrapt(14, isr, RISING);
     dim.write(0);
-
 }
 
 void loop()
 {
-
+    processes.tick();
 }
 
 IRAM_ATTR void isr()
 {
     dim.tick();
+}
+
+void dtpHandler(shs::DTPdata &d)
+{
+    switch (d.data->buf[0])
+    {
+    case shs::chandelierAPI::setBrightness:
+        dim.write(d.data->buf[1]);
+        break;
+
+    default:
+        break;
+    }
 }

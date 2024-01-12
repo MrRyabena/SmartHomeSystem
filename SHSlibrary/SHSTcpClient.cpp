177bc1328748ecf7990526f98a92bda8c1957827
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-01-12 17:19:17 +0300

    Тут пока особо ничего

--- SHSlibrary/SHSTcpClient.cpp
+++ SHSlibrary/SHSTcpClient.cpp
@@ -1,25 +1,10 @@
 #include "SHSTcpClient.h"
 
-shs::TcpClient::TcpClient(const uint8_t *IPaddress, void (*TCPhandle)(shs::DTPdata &), uint16_t _port)
-    : dtp(new shs::DTP((Stream*)&client, TCPhandle))
-{
-    IP = IPaddress;
-    _TCPhandle = TCPhandle;
-    port = _port;
-}
-
-shs::TcpClient::~TcpClient()
-{
-    delete dtp;
-}
-
-void shs::TcpClient::begin()
-{
-    client.connect(IP, port);
-}
-
 void shs::TcpClient::tick()
 {
-    if (client && client.connected())
-        dtp->checkBus(&len);
+    if (!client && !client.connected())
+        return;
+
+    if (millis() - _tmr >= SILENCE_TIMEOUT)
+        ;
 }
--- SHSlibrary/SHSTcpClient.h
+++ SHSlibrary/SHSTcpClient.h
@@ -13,26 +13,14 @@
 namespace shs
 {
     class TcpClient;
+
 };
 
-class shs::TcpClient
+class shs::TcpClient : WiFiClient
 {
 public:
-    WiFiClient client{};
-    shs::DTP *dtp{};
-
-    const uint8_t *IP{};
-    uint16_t port{};
-
-    TcpClient(const uint8_t *IPaddress, void (*TCPhandle)(shs::DTPdata &), uint16_t _port = 50000);
-    ~TcpClient();
-
-    void begin();
     void tick();
-    uint8_t sendPacket(shs::ByteCollector *col, uint8_t id);
-    void sendRAW(uint8_t *buf, uint8_t size);
 
 private:
-    void (*_TCPhandle)(shs::DTPdata &){};
-    uint8_t len{};
+    uint32_t _tmr = millis();
 };

2e24d7ff29f0490ede20cba637f41c317e7a3ce5
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-08 15:28:57 +0300

    Запрещены неявные преобразования

--- SHSlibrary/SHSByteCollector.h
+++ SHSlibrary/SHSByteCollector.h
@@ -2,97 +2,100 @@
 
 namespace shs
 {
-    class ByteCollector
+    class ByteCollector;
+};
+
+
+class shs::ByteCollector
+{
+public:
+    uint8_t *buf{};
+    uint8_t *ptr{};
+    uint8_t *readPtr{};
+
+    explicit ByteCollector(uint8_t size) : buf(new uint8_t[size]{})
     {
-    public:
-        uint8_t *buf{};
-        uint8_t *ptr{};
-        uint8_t *readPtr{};
+        ptr = buf;
+        readPtr = buf;
+        _size = size;
+    }
 
-        ByteCollector(uint8_t size) : buf(new uint8_t[size]{})
-        {
-            ptr = buf;
-            readPtr = buf;
-            _size = size;
-        }
+    ~ByteCollector()
+    {
+        delete[] buf;
+    }
 
-        ~ByteCollector()
-        {
-            delete[] buf;
-        }
+    uint16_t size()
+    {
+        return static_cast<uint16_t>(ptr - buf);
+    }
 
-        uint16_t size()
-        {
-            return static_cast<uint16_t>(ptr - buf);
-        }
+    template <typename T>
+    void add(const T &value, uint8_t bytes = sizeof(T))
+    {
+        if (_size - (ptr - buf) < bytes)
+            reserve(bytes - (_size - (ptr - buf)));
 
-        template <typename T>
-        void add(const T &value, uint8_t bytes = sizeof(T))
-        {
-            if (_size - (ptr - buf) < bytes)
-                reserve(bytes - (_size - (ptr - buf)));
+        uint8_t *_ptr = (uint8_t *)&(value);
+        for (uint8_t i = 0; i < bytes; i++)
+            *ptr++ = *_ptr++;
+    }
 
-            uint8_t *_ptr = (uint8_t *)&(value);
-            for (uint8_t i = 0; i < bytes; i++)
-                *ptr++ = *_ptr++;
-        }
+    template <typename T>
+    void addBefore(const T &value, uint8_t bytes = sizeof(T))
+    {
+        if (!_beforeSize)
+            reserveBefore(bytes);
+        if (_beforeSize - (_ptrBefore - buf) < bytes)
+            reserveBefore(bytes);
+        uint8_t *_ptr = (uint8_t *)&value;
+        for (uint8_t i = 0; i < bytes; i++)
+            *_ptrBefore++ = *_ptr++;
+    }
 
-        template <typename T>
-        void addBefore(const T &value, uint8_t bytes = sizeof(T))
-        {
-            if (!_beforeSize)
-                reserveBefore(bytes);
-            if (_beforeSize - (_ptrBefore - buf) < bytes)
-                reserveBefore(bytes);
-            uint8_t *_ptr = (uint8_t *)&value;
-            for (uint8_t i = 0; i < bytes; i++)
-                *_ptrBefore++ = *_ptr++;
-        }
+    void reserve(uint8_t size)
+    {
+        if (!size)
+            return;
 
-        void reserve(uint8_t size)
-        {
-            if (!size)
-                return;
+        _size += size;
+        uint8_t *n_buf = new uint8_t[_size]{};
+        for (uint16_t i = 0; i < (ptr - buf); i++)
+            n_buf[i] = buf[i];
 
-            _size += size;
-            uint8_t *n_buf = new uint8_t[_size]{};
-            for (uint16_t i = 0; i < (ptr - buf); i++)
-                n_buf[i] = buf[i];
+        ptr = n_buf + (ptr - buf);
+        _ptrBefore = n_buf + (_ptrBefore - buf);
+        delete[] buf;
+        buf = n_buf;
+    }
 
-            ptr = n_buf + (ptr - buf);
-            _ptrBefore = n_buf + (_ptrBefore - buf);
-            delete[] buf;
-            buf = n_buf;
-        }
+    void reserveBefore(uint8_t size)
+    {
+        if (!size)
+            return;
+        _size += size;
+        _beforeSize = size;
+        uint8_t *n_buf = new uint8_t[_size]{};
+        for (uint16_t i = size - 1; i < (ptr - buf) + size; i++)
+            n_buf[i] = buf[i - size];
 
-        void reserveBefore(uint8_t size)
-        {
-            if (!size)
-                return;
-            _size += size;
-            _beforeSize = size;
-            uint8_t *n_buf = new uint8_t[_size]{};
-            for (uint16_t i = size - 1; i < (ptr - buf) + size; i++)
-                n_buf[i] = buf[i - size];
+        ptr = n_buf + size + (ptr - buf);
+        delete[] buf;
+        buf = n_buf;
+        _ptrBefore = buf;
+        readPtr = buf;
+    }
 
-            ptr = n_buf + size + (ptr - buf);
-            delete[] buf;
-            buf = n_buf;
-            _ptrBefore = buf;
-            readPtr = buf;
-        }
+    template <typename T>
+    void get(T &var, uint8_t bytes = sizeof(T))
+    {
+        uint8_t *_ptr = (uint8_t *)&var;
+        for (uint8_t i = 0; i < bytes; i++)
+            *_ptr++ = *readPtr++;
+    }
 
-        template <typename T>
-        void get(T &var, uint8_t bytes = sizeof(T))
-        {
-            uint8_t *_ptr = (uint8_t *)&var;
-            for (uint8_t i = 0; i < bytes; i++)
-                *_ptr++ = *readPtr++;
-        }
-
-    private:
-        uint16_t _size{};
-        uint16_t _beforeSize{};
-        uint8_t *_ptrBefore{};
-        };
+private:
+    uint16_t _size{};
+    uint16_t _beforeSize{};
+    uint8_t *_ptrBefore{};
 };

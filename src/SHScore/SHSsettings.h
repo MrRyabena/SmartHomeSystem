06fc69a2648b164b78978cfe56cecb209916daa6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-15 23:45:56 +0300

    Added namespace shs::settigns and definition SHS_SETTIGNS.

--- src/SHScore/SHSsettings.h
+++ src/SHScore/SHSsettings.h
@@ -1,23 +1,29 @@
-#pragma once
+#ifndef SHS_SETTINGS
+#define SHS_SETTINGS
 
-
+namespace shs::settings
+{
 // WiFi
 #ifndef __AVR__
-//const char* WIFI_SSID = "SHS_WiFi";
 #define WIFI_SSID "SHS_WiFi"
-//const char* WIFI_PASSWORD = "19711970";
 #define WIFI_PASSWORD "19711970"
 #define UPBOT_TOKEN "6956102480:AAGIKbBz_C_Z4Iluz6fZgo2LAfCGVoTYGQg"
+#define TCP_PORT 50000
+#define MACconstant F("SHSma")
 #endif
-
 
 // debug
 #ifdef DEBUG
 
 #define dprint(val) Serial.print(val);
 #define dprintln(val) Serial.println(val);
-#define dprintlln(val) Serial.println(val); Serial.println();
-#define dout(val1, val2) Serial.print(val1); Serial.print(F("  ")); Serial.println(val2);
+#define dprintlln(val)   \
+    Serial.println(val); \
+    Serial.println();
+#define dout(val1, val2)   \
+    Serial.print(val1);    \
+    Serial.print(F("  ")); \
+    Serial.println(val2);
 
 #else
 
@@ -25,5 +31,8 @@
 #define dprintln(val)
 #define dprintlln(val)
 #define dout(val1, val2)
+
+#endif
+};
 
 #endif

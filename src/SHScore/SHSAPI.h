2b55afca96eec54aa5035546efb4e45b3a8ffea7
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-18 14:02:48 +0300

    API "template class" has been edited.

--- src/SHScore/SHSAPI.h
+++ src/SHScore/SHSAPI.h
@@ -1,22 +1,28 @@
+#pragma once
+
+/*
+  Last update: v1.1.0
+  Versions:
+    v1.1.0 — created.
+*/
+
+#include <stdint.h>
+#include "SHSByteCollector.h"
 
 namespace shs
 {
-    template <typename T>
     class API;
 };
 
-template <typename T>
 class shs::API
 {
 public:
-    T *object{};
-    API(T *ptr = nullptr, int16_t ID = 0) : object(ptr), m_ID(ID) {}
+    API(int16_t ID = 0) : m_ID(ID) {}
 
     void setID(int16_t ID) { m_ID = ID; }
     int16_t getID() { return m_ID; }
 
-    template <typename Tdata>
-    virtual void handler(Tdata &data);
+    virtual uint8_t handler(shs::ByteCollector &data);
 
 protected:
     int16_t m_ID{};

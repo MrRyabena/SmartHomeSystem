c7824946a714f04b26abad7ee4ef002d929e0a70
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-18 14:03:34 +0300

    Edited

--- src/SHScore/SHSconfig.h
+++ src/SHScore/SHSconfig.h
@@ -1,9 +1,29 @@
-namespace shs {
-    class Config;
+#pragma once
+
+/*
+  Last update: v1.1.0
+  Versions:
+    v1.1.0 — created.
+*/
+
+#include <stdint.h>
+
+namespace shs
+{
+    struct Config;
+    enum Mode;
 };
 
-class shs::Config
+struct shs::Config
 {
     uint8_t ID{};
-    uint8_t MACaddress[6]{};
+    uint8_t serverID{};
+    shs::Mode mode{};
+};
+
+enum shs::Mode : uint8_t
+{
+    off,
+    standby,
+    sleep,
 };

c2752bb64130ab33f2e4faf9f7f00720a0fb9b12
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-26 00:39:06 +0300

    Unused

--- src/SHScore/SHSupdate.h
+++ src/SHScore/SHSupdate.h
@@ -1,23 +1,23 @@
-#pragma once
-#include "SHSsettings.h"
-#include <FastBot.h>
+// #pragma once
+// #include "SHSsettings.h"
+// #include <FastBot.h>
 
-namespace shs
-{
-    class Update;
-};
+// namespace shs
+// {
+//     class Update;
+// };
 
-class shs::Update
-{
-public:
-    Update();
-    ~Update();
+// class shs::Update
+// {
+// public:
+//     Update();
+//     ~Update();
 
-    void start(const uint32_t timeout = 120000, const bool restart = true);
-    void setHandler(void (*handler)(FB_msg &message));
+//     void start(const uint32_t timeout = 120000, const bool restart = true);
+//     void setHandler(void (*handler)(FB_msg &message));
 
-private:
-    void _newMes(FB_msg &message);
-    void (*_handler)(FB_msg &message, FastBot &upbot) = nullptr;
-};
+// private:
+//     void _newMes(FB_msg &message);
+//     void (*_handler)(FB_msg &message, FastBot &upbot) = nullptr;
+// };
 

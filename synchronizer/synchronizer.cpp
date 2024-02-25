ea195818a077c432131bc80878e5dcfdb1f38172
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-26 00:39:32 +0300

    Paths have been changed

--- synchronizer/synchronizer.cpp
+++ synchronizer/synchronizer.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
-#define LIB "C:/Users/Daniil/Documents/Arduino/libraries/SHS_library"
-#define INP "B:/SmartHomeSystem/SHSlibrary"
-#define LIB2 "C:/Users/Daniil/Documents/Arduino/libraries/devAPI"
-#define INP2 "B:/SmartHomeSystem/devAPI"
+#define LIB "C:/Users/Daniil/Documents/Arduino/libraries/SHScore"
+#define INP "B:/SmartHomeSystem/src/SHScore"
+#define LIB2 "C:/Users/Daniil/Documents/Arduino/libraries/SHSlibrary"
+#define INP2 "B:/SmartHomeSystem/src/SHSlibrary"
 
 int main()
 {
 
-  system("RD /S /Q \"" LIB "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
-  mkdir(LIB);
-  system("xcopy \"" INP "\" \"" LIB "\" /e");
+  while (true)
+  {
+    system("RD /S /Q \"" LIB "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
+    mkdir(LIB);
+    system("xcopy \"" INP "\" \"" LIB "\" /e");
 
-  system("RD /S /Q \"" LIB2 "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
-  mkdir(LIB2);
-  system("xcopy \"" INP2 "\" \"" LIB2 "\" /e");
+    system("RD /S /Q \"" LIB2 "\""); // B:/at work/SmartHomeSystem/synchronizer/core\"");
+    mkdir(LIB2);
+    system("xcopy \"" INP2 "\" \"" LIB2 "\" /e");
 
-  system("pause");
+    system("pause");
+  }
 
   return 0;
 }

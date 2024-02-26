fbc1be77178b5d4eba88455841447c2907217e2d
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-27 00:02:25 +0300

    Tested.

--- src/demo version/SmartModules/SmartServer/SmartServer.ino
+++ src/demo version/SmartModules/SmartServer/SmartServer.ino
@@ -1,12 +1,22 @@
-
+#define _Arduino_
 #include <SHSsettings_private.h>
 #include <SHSTcpServer.h>
+#include <SHSLoadAPI.h>
 #include <SHSModule.h>
 #include <SHSconnectWiFi.h>
 
+
+
 shs::TcpServer server(shs::settings::COM_IP);
 
+void sendPacket(shs::ByteCollector *bc, const shs::settings::shs_ModuleID_t to, const shs::settings::shs_ID_t api_ID) {
+  server.sendPacket(bc, to, api_ID);
+}
+
+
+
 void setup() {
+  shs::module.sendPacket = sendPacket;
   shs::module.config.ID = 4;
   shs::module.config.serverID = 4;
   shs::module.config.mode = shs::Mode::on;

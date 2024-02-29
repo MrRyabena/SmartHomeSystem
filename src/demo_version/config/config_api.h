ee76c2378b25695f865e9ef354b39c65f37d7b89
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-02-29 23:54:02 +0300

    Created as fast simple version.

---
+++ src/demo_version/config/config_api.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "../../SHScore/SHSsettings_private.h"
+
+namespace shs::config
+{
+
+    namespace SmartModulesID
+    {
+        enum SmartModulesID : shs::settings::shs_ModuleID_t
+        {
+            SmartServer = 4,
+            SmartBulb = 5,
+            SmartLight = 6,
+            SmartFan = 7,
+            App = 10,
+        };
+    };
+
+    namespace APIID
+    {
+        enum APIID : shs::settings::shs_ID_t
+        {
+            
+        };
+    };
+
+    namespace SmartServerAPI
+    {
+        enum SmartServerAPI : uint8_t
+        {
+            getReport,
+            report,
+
+            getStatus,
+            status,
+
+            getMode,
+            mode,
+
+            getTherm,
+            therm,
+
+            getPhoto,
+            photo,
+
+            setFan,
+            setLed,
+            setBulb,
+            setHalogen,
+        };
+    };
+};

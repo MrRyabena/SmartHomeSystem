89133fb0e6e774cd5431564e029d8cc82963587a
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-03-03 16:44:41 +0300

    Desctiption added. Ready for release.

--- src/SHScore/SHSStream.h
+++ src/SHScore/SHSStream.h
@@ -1,4 +1,18 @@
 #pragma once
+
+/*
+  Last update: v1.0.0
+  Versions:
+    v0.2.0 — created.
+    v1.0.0 — release.
+*/
+
+/*
+  An abstract class to replace the Arduino Serial. Needed for DTP.
+*/
+
+#if __has_include(<Arduino.h>)
+#else
 #include <stdint.h>
 
 namespace shs
@@ -9,7 +23,8 @@ namespace shs
 class shs::Stream
 {
 public:
-    virtual uint8_t write(const uint8_t *buf, size_t size) = 0;
+    virtual uint8_t write(const uint8_t *buf, uint16_t size) = 0;
     virtual uint8_t read() = 0;
     virtual uint8_t available() = 0;
 };
+#endif

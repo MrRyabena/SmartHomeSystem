c5d8204cd491233cc193d315228ffc381219a707
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-03-03 16:26:39 +0300

    Corrected.

--- src/SHScore/SHSSensorAnalog.cpp
+++ src/SHScore/SHSSensorAnalog.cpp
@@ -1,9 +1,9 @@
 #include "SHSSensorAnalog.h"
 
-shs::SensorAnalog::SensorAnalog(const shs::settings::shs_ID_t ID, const shs::SensorType stype, const uint8_t pin)
+shs::SensorAnalog::SensorAnalog(const shs::settings::shs_ID_t ID, const shs::SensorType::SensorType stype, const uint8_t pin)
     : ::shs::Sensor(ID, stype), m_pin(pin) {}
 
-void shs::SensorAnalog::setup() { pinMode(m_pin, INPUT); }
+inline void shs::SensorAnalog::setup() { pinMode(m_pin, INPUT); }
 
 int16_t shs::SensorAnalog::getValueI(const shs::settings::shs_ID_t ID) { return analogRead(m_pin); }
 shs::settings::shs_float_t shs::SensorAnalog::getValueF(const shs::settings::shs_ID_t ID) { return static_cast<shs::settings::shs_float_t>(analogRead(m_pin)); }
@@ -23,6 +23,12 @@ shs::settings::shs_double_t shs::SensorAnalog::getAverageD(const shs::settings::
 {
     return (shs::settings::shs_double_t)m_getSamplesSum() / shs::settings::SENSOR_AVERAGE_SAMPLES;
 }
+
+/*
+  ----------------------------------------
+  PRIVATE/PROTECTED
+  ----------------------------------------
+*/
 
 uint16_t shs::SensorAnalog::m_getSamplesSum(const uint8_t count)
 {

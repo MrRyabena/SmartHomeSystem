1876a15b4dbe28483388796bce73f0ac96da7c1c
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-03-03 15:59:24 +0300

    inline added.

--- src/SHScore/SHSSensor.cpp
+++ src/SHScore/SHSSensor.cpp
@@ -1,8 +1,8 @@
 #include "SHSSensor.h"
 
-shs::Sensor::Sensor(const shs::settings::shs_ID_t ID, const shs::SensorType stype)
- : m_ID(ID), type(stype) {}
+shs::Sensor::Sensor(const shs::settings::shs_ID_t ID, const shs::SensorType::SensorType stype)
+    : m_sensorID(ID), type(stype) {}
 
-void shs::Sensor::setID(const shs::settings::shs_ID_t ID) { m_ID = ID; }
+inline void shs::Sensor::setID(const shs::settings::shs_ID_t ID) { m_sensorID = ID; }
 
-shs::settings::shs_ID_t shs::Sensor::getID() const  { return m_ID; }
+inline shs::settings::shs_ID_t shs::Sensor::getID() const { return m_sensorID; }

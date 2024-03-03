18e7f7cb42c9657c52ba29f7ecef83b4ec67ed40
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-03-03 16:26:51 +0300

    Descriprion added.

--- src/SHScore/SHSSensorAnalog.h
+++ src/SHScore/SHSSensorAnalog.h
@@ -1,5 +1,17 @@
 #pragma once
 
+/*
+  Last update: v1.0.0
+  Versions:
+    v0.1.0 — created.
+    v0.2.0 — made purely virtual.
+    v1.0.0 — release.
+*/
+
+/*
+  A class for getting values from analog pins to which sensors are connected.
+*/
+
 #include "SHSSensor.h"
 #include "SHSsettings_private.h"
 #include <Arduino.h>
@@ -12,7 +24,7 @@ namespace shs
 class shs::SensorAnalog : public shs::Sensor
 {
 public:
-    explicit SensorAnalog(const shs::settings::shs_ID_t ID = 0, const shs::SensorType stype = shs::SensorType::unknown, const uint8_t pin = A0);
+    explicit SensorAnalog(const shs::settings::shs_ID_t ID = 0, const shs::SensorType::SensorType stype = shs::SensorType::unknown, const uint8_t pin = A0);
 
     void setup() override;
     shs::settings::shs_float_t getValueF(const shs::settings::shs_ID_t ID = 0) override;

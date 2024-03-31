b6c9fd82156acbc7a57187bcaffdbdfafc5b23b9
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-03-31 10:57:57 +0300

    Created for transferring libraries. Replaced the synchronizer.

---
+++ src/make.sh
@@ -0,0 +1,20 @@
+# This is a simple file for transferring libraries to the Arduino environment.
+
+# Last update: v1.1.0
+#  Versions:
+#    v1.1.0 — created.
+#
+
+# Уou must specify the path to the folder here:
+ArduinoLibraries=/mnt/c/Users/Daniil/Documents/Arduino/libraries/
+
+echo "ArduinoLibraries directory is: $ArduinoLibraries"
+
+rm -rf "${ArduinoLibraries} SHScore/"
+cp -r ./SHScore/ "${ArduinoLibraries} SHScore/"
+
+rm -rf  "${ArduinoLibraries} SHSlibrary"
+cp -r ./SHSlibrary/ "${ArduinoLibraries} SHSlibrary/"
+
+rm -rf "${ArduinoLibraries} SHStests"
+cp -r ./SHStests/source/ "${ArduinoLibraries} SHStests/"

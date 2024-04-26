63a0ccc855cd6f4a6646ebe0444d89f13dcd1d75
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-04-26 18:24:19 +0300

    Added shs_string_t.

--- src/SHScore/SHSsettings_private.h
+++ src/SHScore/SHSsettings_private.h
@@ -4,31 +4,42 @@
 
 #include <stdint.h>
 
+#if __has_include(<Arduino.h>)
+#include <Arduino.h>
+#define ARDUINO
+#else
+#include <string>
+#endif
+
 #define DEBUG
 
 namespace shs
 {
-    namespace settings
-    {
+        namespace settings
+        {
 #ifndef USE_FLOAT_FOR_DOUBLE
-        using shs_double_t = double;
+                using shs_double_t = double;
 #else
-        using shs_double_t = float;
+                using shs_double_t = float;
 #endif
 
-        using shs_float_t = float;
-        using shs_ModuleID_t = uint8_t;
-        using shs_ID_t = int16_t;
+#ifdef ARDUINO
+                using shs_string_t = String;
+#else
+                using shs_string_t = std::string;
+#endif
 
-        const uint8_t SENSOR_AVERAGE_SAMPLES = 20;
-        // const uint8_t COM_IP[] = {192, 168, 1, 4};
-        const uint8_t SERVER_ID = 4;
+                using shs_float_t = float;
+                using shs_ModuleID_t = uint8_t;
+                using shs_ID_t = int16_t;
 
-        const int8_t Load_apiID = -1;
-        const int8_t Sensor_apiID = -2;
+                const uint8_t SENSOR_AVERAGE_SAMPLES = 20;
+                // const uint8_t COM_IP[] = {192, 168, 1, 4};
+                const uint8_t SERVER_ID = 4;
 
+                const int8_t Load_apiID = -1;
+                const int8_t Sensor_apiID = -2;
 
-
-    }
+        }
 }
 #endif

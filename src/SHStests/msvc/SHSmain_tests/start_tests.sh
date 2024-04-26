30a678cb33b49efbb2de041f149e18788c0b4e91
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-04-26 18:27:53 +0300

    Updated.

--- src/SHStests/msvc/SHSmain_tests/start_tests.sh
+++ src/SHStests/msvc/SHSmain_tests/start_tests.sh
@@ -1,10 +1,10 @@
 # It was created for automatic assembly and test run.
-
+#
 # Last update: v1.1.0
 #  Versions:
 #    v1.1.0 — created.
 
-mkdir build
+mkdir -p build
 cd build
 cmake ..
 cmake --build .

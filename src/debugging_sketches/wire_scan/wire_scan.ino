9835baf97a0b6b733837741d19e61dd005a0add1
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-06-14 21:24:24 +0300

    Corrected.

--- src/debugging_sketches/wire_scan/wire_scan.ino
+++ src/debugging_sketches/wire_scan/wire_scan.ino
@@ -3,11 +3,11 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Start scan");
 
-  Wire.begin(2, 0);
+  Wire.begin();
 }
 
 void loop() {
-  for (uint8_t i = 0; i < 255; i++) {
+  for (uint8_t i = 1; i < 128; i++) {
     Wire.beginTransmission(i);
     if (!Wire.endTransmission()) Serial.println(i, HEX);
   }

10e6560f76283410aeb4980aa643e8ae0031dc7c
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-06-14 21:35:08 +0300

    Corrected.

--- src/SmartModules [beta]/WeatherStation/WeatherStation.ino
+++ src/SmartModules [beta]/WeatherStation/WeatherStation.ino
@@ -1,14 +1,41 @@
-#include <SHSconnectWiFi.h>
+#include <shs_ControlWiFi.h>
+#include "presets.h"
+#include <LittleFS.h>
 
-#include "TemporaryWeatherStation.h"
-shs::TemporaryWeatherStation tws;
+#include <GyverNTP.h>
+
+#include <GyverBME280.h>
+#include <DHT.h>
+#include <shs_WeatherStationData.h>
+
+#include <shs_CRC.h>
+
+
+GyverNTP ntp(3);
+
+GyverBME280 bme;
+DHT dht(DHTpin, DHT22);
+
+
+void WSupdate(shs::WeatherStationData &wsd);
+void WSsave(shs::WeatherStationData &wsd);
+
+void WSbegin();
+void WStick();
+
+uint32_t WS_T = 600'000;
 
 void setup()
 {
-    if (tws.begin());
+    shs::ControlWiFi::configureWiFi();
+    shs::ControlWiFi::connectWiFi();
+    delay(5000);
+    WSbegin();
 }
 
 void loop()
 {
-
+    ntp.tick();
+    telegramBot();
+    WStick();
 }
--- src/SmartModules [beta]/chandelier_temp/bot.ino
+++ src/SmartModules [beta]/chandelier_temp/bot.ino
@@ -8,7 +8,8 @@ FastBot bot(CHANDELIER_BOT_TOKEN);
 
 void handleBot(FB_msg &message);
 
-void botSetup() {
+void botSetup()
+{
 
 #ifdef BOT_UPDATE_TIME
   bot.setPeriod(BOT_UPDATE_TIME);
@@ -23,11 +24,13 @@ void botSetup() {
   Serial.println("ok");
 }
 
-void telegramBot() {
+void telegramBot()
+{
 
 #ifdef BOT_ACTIVE
   static bool botSetup_flag{};
-  if (shs::ControlWiFi::WiFiConnected() && !botSetup_flag) {
+  if (shs::ControlWiFi::WiFiConnected() && !botSetup_flag)
+  {
     botSetup_flag = true;
     botSetup();
   }
@@ -36,7 +39,8 @@ void telegramBot() {
 #endif
 }
 
-void botSendNotification(String message) {
+void botSendNotification(String message)
+{
 #ifdef BOT_ACTIVE
 
   bot.sendMessage("<b><u><i>SYSTEM NOTIFICATION!</i></u>\n" + message + "</b>", CHAT_ID);
@@ -47,31 +51,38 @@ void botSendNotification(String message) {
 
 
 
-void handleBot(FB_msg &message) {
-  if (message.OTA && message.text.equals(F("/OTA"))) {
-    if (message.fileName.indexOf("mklittlefs") > 0 || message.fileName.indexOf("spiffs") > 0) {
+void handleBot(FB_msg &message)
+{
+  if (message.OTA && message.text.equals(F("/OTA")))
+  {
+    if (message.fileName.indexOf("mklittlefs") > 0 || message.fileName.indexOf("spiffs") > 0)
+    {
       bot.updateFS();  // update spiffs
-    } else {
+    } else
+    {
       bot.update();  // update sketch
     }
   }
 
-  if (message.text.indexOf('@') != -1)
-    if (message.text.indexOf(BOT_NAME) != -1) message.text.remove(0, strlen(BOT_NAME));
+  auto ind = message.text.indexOf('@');
+  if (ind != -1)
+    if (message.text.indexOf(BOT_NAME) != -1) message.text.remove(ind, strlen(BOT_NAME + 1));
   if (message.text.startsWith("/")) message.text.remove(0, 1);
 
 
-  if (message.text.startsWith("part")) {
+  if (message.text.startsWith("part"))
+  {
     String str = message.text.substring(message.text.indexOf(' '));
     uint8_t val = static_cast<uint8_t>(str.toInt());
 
-    switch (val) {
-      case 1: digitalWrite(PART1_pin, 1); break;
-      case 2: digitalWrite(PART2_pin, 1); break;
-      default:
-        digitalWrite(PART1_pin, 0);
-        digitalWrite(PART2_pin, 0);
-        break;
+    switch (val)
+    {
+    case 1: digitalWrite(PART1_pin, 1); break;
+    case 2: digitalWrite(PART2_pin, 1); break;
+    default:
+      digitalWrite(PART1_pin, 0);
+      digitalWrite(PART2_pin, 0);
+      break;
     }
 
     String answer = "Part: ";
@@ -80,19 +91,22 @@ void handleBot(FB_msg &message) {
     bot.sendMessage(answer, message.chatID);
   }
 
-  else if (message.text.startsWith("on")) {
+  else if (message.text.startsWith("on"))
+  {
     String str = "on!";
     digitalWrite(PART1_pin, 1);
     digitalWrite(PART2_pin, 1);
 
     bot.sendMessage(str, message.chatID);
-  } else if (message.text.startsWith("off")) {
+  } else if (message.text.startsWith("off"))
+  {
     String str = "off!";
     digitalWrite(PART1_pin, 0);
     digitalWrite(PART2_pin, 0);
 
     bot.sendMessage(str, message.chatID);
-  } else if (message.text.startsWith("temp")) {
+  } else if (message.text.startsWith("temp"))
+  {
     String str = "Temp: ";
     str += therm.getTempAverage();
     str += "*C.\n";

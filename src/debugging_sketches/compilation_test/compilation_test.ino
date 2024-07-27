5286d1d9aeedd7c6313471e7955e11c6cd6cbc3f
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-07-27 22:48:43 +0300

    Created for fast test.

---
+++ src/debugging_sketches/compilation_test/compilation_test.ino
@@ -0,0 +1,21 @@
+#include <shs_API.h>
+#include <shs_algorithm.h>
+#include <shs_ByteCollector.h>
+#include <shs_ControlWiFi.h>
+#include <shs_CRC.h>
+#include <shs_ID.h>
+#include <shs_Process.h>
+#include <shs_settings.h>
+#include <shs_types.h>
+
+void setup() 
+{
+
+}
+
+
+void loop() 
+{
+
+}
+

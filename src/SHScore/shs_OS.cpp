68e72c83f8e41bc3fc4d49f072e76e40e4883903
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-09-16 00:24:44 +0300

    Revision and debugging are required.

--- src/SHScore/shs_OS.cpp
+++ src/SHScore/shs_OS.cpp
@@ -2,67 +2,55 @@
 
 shs::OS::OS(const uint8_t cores, const uint8_t threads, const size_t thread_stack_size)
     : m_cores(cores),
-      m_threads(threads)
+    m_threads(threads)
 {
 
-    m_processes.reserve(cores * threads);
+    m_handlers.reserve(cores * threads);
 
     for (uint8_t core = 0; core < m_cores; core++)
         for (uint8_t thread = 0; thread < m_threads; thread++)
         {
 
             auto ph = std::make_unique<shs::ProcessesHandler>();
-            auto task = std::make_shared<shs::ProcessTask>(
-                ph.get(),
-                0,
-                1,
-                core,
-                thread_stack_size);
 
-                std::cout << "Test" << std::endl;
-            auto thr = std::make_unique<std::thread>(shs::ProcessTask::processFunction, task);
-            
-            
+            auto id = m_tm.addTask<shs::ProcessesHandler>(shs::ProcessTask(ph.get()));
+            m_handlers.push_back(std::pair<std::unique_ptr<shs::Process>, shs::task::task_id_t>(std::move(ph), id));
 
-            
-            //m_tm.addTask(*task);
-            m_processes.push_back(std::tuple<std::unique_ptr<shs::ProcessesHandler>, std::shared_ptr<shs::ProcessTask>, std::unique_ptr<std::thread>>(std::move(ph), task, std::move(thr)));
-            
         }
 }
 
 
-shs::OS::~OS() 
+shs::OS::~OS()
 {
-    for (auto &x : m_processes) std::get<1>(x)->m_running = false;
+    for (auto& x : m_handlers) static_cast<shs::ProcessTask*>(m_tm[x.second]->task.get())->setStop();
     //while (m_tm.getTaskCount()) m_tm.checkCompleted();
 }
 
 
-void shs::OS::addProcess(shs::Process *process, const uint8_t coreID, const uint8_t threadID)
+void shs::OS::addProcess(shs::Process* process, const uint8_t coreID, const uint8_t threadID)
 {
-    if (coreID == 0xff)
-    {
-        uint8_t min_ind{};
-        uint16_t min_load = 0xffff;
+    // if (coreID == 0xff)
+    // {
+    //     uint8_t min_ind{};
+    //     uint16_t min_load = 0xffff;
 
-        for (uint8_t i = 0; i < m_processes.size(); i++)
-        {
-            if (std::get<0>(m_processes[i])->size() < min_load)
-            {
-                min_load == std::get<0>(m_processes[i])->size();
-                min_ind = i;
-            }
-        }
-      
+    //     for (uint8_t i = 0; i < m_processes.size(); i++)
+    //     {
+    //         if (std::get<0>(m_processes[i])->size() < min_load)
+    //         {
+    //             min_load == std::get<0>(m_processes[i])->size();
+    //             min_ind = i;
+    //         }
+    //     }
 
-        std::get<0>(m_processes[min_ind])->attach(process);
-    }
+
+    //     std::get<0>(m_processes[min_ind])->attach(process);
+    // }
 }
 
 void shs::OS::stop()
 {
     std::cout << "STOP" << std::endl;
-    for (auto &x : m_processes) std::get<1>(x)->setStop();
-    for (auto &x : m_processes) std::get<2>(x)->join();
+    for (auto& x : m_handlers) static_cast<shs::ProcessTask*>(m_tm[x.second]->task.get())->setStop();
+    for (auto& x : m_handlers) m_tm[x.second]->handle->join();
 }
--- src/SHScore/shs_OS.h
+++ src/SHScore/shs_OS.h
@@ -24,17 +24,16 @@ public:
     explicit OS(const uint8_t cores = 1, const uint8_t threads = 1, const size_t thread_stack_size = 1000);
     ~OS();
 
-    void addProcess(shs::Process *process, const uint8_t coreID = 0xff, const uint8_t threadID = 0xff);
+    void addProcess(shs::Process* process, const uint8_t coreID = 0xff, const uint8_t threadID = 0xff);
 
     void stop();
 
 private:
     const uint8_t m_cores;
     const uint8_t m_threads;
-    
-    std::vector<std::tuple<std::unique_ptr<shs::ProcessesHandler>, std::shared_ptr<shs::ProcessTask>, std::unique_ptr<std::thread>>> m_processes;
-    //shs::TaskManager m_TM;
-    // static constexpr void (*sleep_for)(size_t value) = std::this_thread::sleep_for(std::chrono::milliseconds(value))
+
+    std::vector<std::pair<std::unique_ptr<shs::Process>, shs::task::task_id_t>> m_handlers;
+
 public:
-    //shs::TaskManager m_tm;
+    shs::TaskManager m_tm;
 };

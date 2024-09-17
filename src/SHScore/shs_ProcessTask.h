e912961c1a6dc2ffec5fcb6a7f7fe001d95dd452
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-09-17 23:11:31 +0300

    Fixed critical errors when copying and constructing an object.

--- src/SHScore/shs_ProcessTask.h
+++ src/SHScore/shs_ProcessTask.h
@@ -4,38 +4,43 @@
 #include "shs_Process.h"
 #include <memory>
 
-
 namespace shs
 {
     class ProcessTask;
 }
 
-
-
 class shs::ProcessTask : public shs::Task
 {
 public:
-    ProcessTask(shs::Process* process)
-        : Task(ProcessTask::processFunction, static_cast<void*>(this)),
-        m_process(process) {}
+    ProcessTask(shs::Process *process)
+        : Task(ProcessTask::processFunction, static_cast<void *>(this)),
+          m_process(process), m_running(false) {}
+
+    ProcessTask(const ProcessTask &other)
+        : Task(ProcessTask::processFunction, static_cast<void *>(this)),
+          m_process(other.m_process),
+          m_running(other.m_running) {}
+
+    ProcessTask &operator=(const ProcessTask &) = delete;
 
     ~ProcessTask() { std::cout << "~ProcessTask()" << std::endl; }
 
     void start() { m_process->start(); }
     void stop() { m_process->stop(); }
 
-    void setStop() {
+    void setStop()
+    {
         std::cout << "Stopping process..." << std::endl;
         m_running = false; // Убедитесь, что это состояние действительно меняется
     }
 
 private:
-    shs::Process* m_process;
-    bool m_running = false;
+    shs::Process *m_process;
+    bool m_running;
 
-    static void processFunction(void* param)
+    static void processFunction(void *param)
     {
-        auto self = static_cast<shs::ProcessTask*>(param);
+        auto self = static_cast<shs::ProcessTask *>(param);
         self->m_running = true;
         self->start();
         while (self->m_running)
@@ -51,5 +56,3 @@ private:
     friend class TaskManager;
     friend class OS;
 };
-
-

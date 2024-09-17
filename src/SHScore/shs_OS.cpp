55847d4987ebac19c18ac088662d1ba6af034d9f
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-09-17 23:10:05 +0300

    It works, requires testing on microcontrollers and improvement of functionality

--- src/SHScore/shs_OS.cpp
+++ src/SHScore/shs_OS.cpp
@@ -2,7 +2,7 @@
 
 shs::OS::OS(const uint8_t cores, const uint8_t threads, const size_t thread_stack_size)
     : m_cores(cores),
-    m_threads(threads)
+      m_threads(threads)
 {
 
     m_handlers.reserve(cores * threads);
@@ -14,43 +14,42 @@ shs::OS::OS(const uint8_t cores, const uint8_t threads, const size_t thread_stac
             auto ph = std::make_unique<shs::ProcessesHandler>();
 
             auto id = m_tm.addTask<shs::ProcessesHandler>(shs::ProcessTask(ph.get()));
-            m_handlers.push_back(std::pair<std::unique_ptr<shs::Process>, shs::task::task_id_t>(std::move(ph), id));
-
+            m_handlers.push_back(std::pair<std::unique_ptr<shs::ProcessesHandler>, shs::task::task_id_t>(std::move(ph), id));
         }
 }
 
-
 shs::OS::~OS()
 {
-    for (auto& x : m_handlers) static_cast<shs::ProcessTask*>(m_tm[x.second]->task.get())->setStop();
-    //while (m_tm.getTaskCount()) m_tm.checkCompleted();
+    for (auto &x : m_handlers)
+        static_cast<shs::ProcessTask *>(m_tm[x.second]->task.get())->setStop();
+    // while (m_tm.getTaskCount()) m_tm.checkCompleted();
 }
 
-
-void shs::OS::addProcess(shs::Process* process, const uint8_t coreID, const uint8_t threadID)
+void shs::OS::addProcess(shs::Process *process, const uint8_t coreID, const uint8_t threadID)
 {
-    // if (coreID == 0xff)
-    // {
-    //     uint8_t min_ind{};
-    //     uint16_t min_load = 0xffff;
+    if (coreID == 0xff)
+    {
+        uint8_t min_ind{};
+        uint16_t min_load = 0xffff;
 
-    //     for (uint8_t i = 0; i < m_processes.size(); i++)
-    //     {
-    //         if (std::get<0>(m_processes[i])->size() < min_load)
-    //         {
-    //             min_load == std::get<0>(m_processes[i])->size();
-    //             min_ind = i;
-    //         }
-    //     }
+        for (uint8_t i = 0; i < m_handlers.size(); i++)
+        {
+            if (m_handlers[i].first->size() < min_load)
+            {
+                min_load == m_handlers[i].first->size();
+                min_ind = i;
+            }
+        }
 
-
-    //     std::get<0>(m_processes[min_ind])->attach(process);
-    // }
+        m_handlers[min_ind].first->attach(process);
+    }
 }
 
 void shs::OS::stop()
 {
     std::cout << "STOP" << std::endl;
-    for (auto& x : m_handlers) static_cast<shs::ProcessTask*>(m_tm[x.second]->task.get())->setStop();
-    for (auto& x : m_handlers) m_tm[x.second]->handle->join();
+    for (auto &x : m_handlers)
+        static_cast<shs::ProcessTask *>(m_tm[x.second]->task.get())->setStop();
+    for (auto &x : m_handlers)
+        m_tm[x.second]->handle->join();
 }
--- src/SHScore/shs_OS.h
+++ src/SHScore/shs_OS.h
@@ -24,16 +24,14 @@ public:
     explicit OS(const uint8_t cores = 1, const uint8_t threads = 1, const size_t thread_stack_size = 1000);
     ~OS();
 
-    void addProcess(shs::Process* process, const uint8_t coreID = 0xff, const uint8_t threadID = 0xff);
+    void addProcess(shs::Process *process, const uint8_t coreID = 0xff, const uint8_t threadID = 0xff);
 
     void stop();
 
 private:
+    shs::TaskManager m_tm;
     const uint8_t m_cores;
     const uint8_t m_threads;
 
-    std::vector<std::pair<std::unique_ptr<shs::Process>, shs::task::task_id_t>> m_handlers;
-
-public:
-    shs::TaskManager m_tm;
+    std::vector<std::pair<std::unique_ptr<shs::ProcessesHandler>, shs::task::task_id_t>> m_handlers;
 };

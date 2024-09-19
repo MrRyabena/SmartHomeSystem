180af008bcd66a7dd9fcf5ecd548918e289870c2
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-09-19 23:40:22 +0300

    Completed.

--- src/SHScore/shs_DTP.cpp
+++ src/SHScore/shs_DTP.cpp
@@ -1,55 +1,5 @@
-#include "SHSdtp.h"
 #include "shs_DTP.h"
 
-/*
-  ----------------------------------------
-  DTPpacker
-  ----------------------------------------
-*/
-
-
-uint8_t shs::DTPpacker::packDTP(shs::DTPpacket& packet)
-{
-    packet.bc.reserve(1);
-    packet.bc.reserve_front(DTP_OFFSETbeg);
-
-
-    // note: reversed order
-    bc.push_front(recipientID);
-    bc.push_front(senderID);
-    bc.push_front(size + DTP_OFFSETbeg + 1, 1);
-
-
-    bc.push_back(shs::CRC8::crcBuf(bc.getPtr(), bc.size() - 1), 1);
-
-    return bc.buf[0];
-}
-
-uint8_t shs::DTPpacker::checkDTP(const shs::ByteCollector<>* bc)
-{
-    if (bc->size() < bc->buf[0]) return Error::size_less;
-    if (bc->size() > bc->buf[0]) return Error::size_bigger;
-
-    if ((*bc)[(*bc)[0] - 1] != shs::CRC8::crcBuf(bc->getPtr(), (*bc)[0] - 1))
-        return Error::invalid_crc;
-
-    return Error::ok;
-}
-
-uint8_t shs::DTPpacker::parseDTP(shs::ByteCollector<>&& bc, shs::DTPpacket& packet)
-{
-    auto status = checkDTP(bc);
-    if (status) return status;
-
-    bc.get(packet.datasize, 1);
-    bc.get(packet.senderID);
-    bc.get(packet.recipientID);
-
-    packet.datasize -= DTP_OFFSETbeg + 1;
-    packet.bc = std::move(bc);
-
-    return Error::ok;
-}
 
 /*
   ----------------------------------------
@@ -57,60 +7,36 @@ uint8_t shs::DTPpacker::parseDTP(shs::ByteCollector<>&& bc, shs::DTPpacket& pack
   ----------------------------------------
 */
 
-shs::DTP::DTP(Stream* bus, const shs::settings::shs_ModuleID_t ID) : m_bus(bus), m_ID(ID) {}
-
-shs::DTP::~DTP() {}
-
-uint8_t shs::DTP::tick()
+uint8_t shs::DTP::checkBus()
 {
-    checkBus();
-    return 0;
-}
+    if (m_bus.available() == 0) return Status::no_data;
 
-uint8_t shs::DTP::checkBus(uint8_t* len)
-{
-    if (!m_bus)
-        return 0;
-    if (len)
-        m_len = *len;
-
-    if (m_bus->available() > 0)
-        if (!m_len)
-            m_len = m_bus->read();
-    if (len)
-        *len = m_len;
-    if (!m_len)
-        return 0;
-
-    if (m_len && m_bus->available() < m_len - 1)
-        return 1;
+    if (m_len == 0) m_len = m_bus.read();
+    if (m_bus.available() < m_len - 1) return Status::packet_is_expected;
 
     shs::ByteCollector col(m_len);
-    *(col.ptr++) = m_len;
+    col.push_back(m_len, 1);
 
-    for (uint8_t i = 0; i < m_len - 1; i++)
-        *(col.ptr++) = m_bus->read();
+    for (uint8_t i = 0; i < m_len - 1; i++) col.push_back(m_bus.read());
+
     m_len = 0;
-    if (len)
-        *len = 0;
 
-    // shs::DTPdata data;
-    // uint8_t size = parseDTP(&col, data);
-
-    handler(col);
-
-    return col.size();
+    return m_handle_packet(std::move(col));
 }
 
-uint8_t shs::DTP::sendPacket(shs::ByteCollector* bc, const shs::settings::shs_ModuleID_t to)
+
+uint8_t shs::DTP::m_handle_packet(shs::ByteCollector<>&& bc)
 {
-    return shs::DTP::sendPacket(bc, to, m_ID, m_ID);
-}
+    shs::DTPpacket packet(std::move(bc));
 
-uint8_t shs::DTP::sendPacket(shs::ByteCollector* bc, const shs::settings::shs_ModuleID_t to,
-    const shs::settings::shs_ID_t api_ID, const shs::settings::shs_ModuleID_t from)
-{
-    packDTP(bc, to, api_ID, from);
-    return m_bus->write(bc->buf, bc->buf[0]);
-}
+    auto id = packet.get_recipientID();
+   
+    auto handler = shs::binary_search(begin(), end(), id, shs::DTPless::APIid());
+    
 
+    if (handler == end()) return Status::invalid_recipient;
+
+    (*handler)->handle(packet);
+
+    return Status::packet_processed;
+}
--- src/SHScore/shs_DTP.h
+++ src/SHScore/shs_DTP.h
@@ -40,12 +40,16 @@ using Stream = shs::Stream;
 
 #include "shs_settings_private.h"
 #include "shs_types.h"
-#include "shs_CallbacksKeeper.h"
 #include "shs_CRC.h"
 #include "shs_ByteCollector.h"
 #include "shs_ID.h"
-
+#include "shs_SortedBuf.h"
+#include "shs_Process.h"
+#include "shs_API.h"
+#include "shs_algorithm.h"
+#include "shs_DTPpacket.h"
 #include <stdint.h>
+#include "functional"
 
 namespace shs::settings
 {
@@ -62,16 +66,13 @@ namespace shs
         enum DTPcommands : uint8_t;
     };
 
-    namespace DTPhandlerStatus
-    {
-        enum DTPhandlerStatus : uint8_t;
-    };
-
-    struct DTPpacket;
     class DTP;
-    class DTPpacker;
-    typedef void (*DTPhandler_t)(shs::DTPdata&);
 
+    namespace DTPless
+    {
+        struct APIptr;
+        struct APIid;
+    }
 };
 
 enum shs::DTPcommands::DTPcommands : uint8_t
@@ -81,99 +82,44 @@ enum shs::DTPcommands::DTPcommands : uint8_t
     request,
 };
 
-struct shs::DTPpacket
+
+struct shs::DTPless::APIptr { bool operator()(const shs::API* lhs, const shs::API* rhs) const { return lhs->API_ID < rhs->API_ID; } };
+struct shs::DTPless::APIid
 {
-
-    explicit DTPpacket(
-        const shs::t::shs_ID_t set_senderID,
-        const shs::t::shs_ID_t set_recipientID,
-        shs::ByteCollector<>&& set_data
-    )
-        :
-        senderID(set_senderID), recipientID(set_recipientID),
-        bc(std::move(set_data))
-    {}
-
-    explicit DTPpacket(
-        const shs::t::shs_ID_t set_senderID,
-        const shs::t::shs_ID_t set_recipientID,
-        const uint8_t* data,
-        const uint8_t size
-    )
-        :
-        senderID(set_senderID), recipientID(set_recipientID),
-        datasize(size), bc(size)
-    {
-        bc.write(data, size);
-    }
-
-
-    DTPpacket(shs::ByteCollector<>&& bc_data)
-        : bc(std::move(bc_data))
-    {
-        shs::DTPpacker::parseDTP(&bc, *this);
-    }
-
-    shs::t::shs_ID_t senderID{};
-    shs::t::shs_ID_t recipientID{};
-    shs::ByteCollector<> bc;
-    uint8_t datasize{};
-
+    bool operator()(const shs::API* lhs, const shs::t::shs_ID_t rhs) const { return lhs->API_ID < rhs; }
+    bool operator()(const shs::t::shs_ID_t lhs, const shs::API* rhs) const { return lhs < rhs->API_ID; }
 };
 
-/*
-  ----------------------------------------
-  DTPpacker
-  ----------------------------------------
-*/
-
-/*
-  0x00  1B  -- message size
-  0x01  4B  -- sender's ID
-  0x05  4B  -- recipient's ID
-  0x09  xB  -- data
-  0xXX  1B  -- CRC
-*/
-class shs::DTPpacker
-{
-    public:
-
-    static uint8_t packDTP(shs::DTPpacket &packet);
-    static uint8_t checkDTP(const shs::ByteCollector<>* bc);
-    uint8_t parseDTP(shs::ByteCollector<>&& bc, shs::DTPpacket& packet);
-
-    static constexpr auto DTP_OFFSETbeg = 10;
-
-    static enum Error : uint8_t
-    {
-        ok,
-        size_less,
-        size_bigger,
-        invalid_crc
-    };
-
-};
 
 /*
   ----------------------------------------
   DTP
   ----------------------------------------
 */
-class shs::DTP : public shs::DTPpacker, public shs::CallbacksKeeper
+class shs::DTP : public shs::Process, public shs::SortedBuf<shs::API*, shs::DTPless::APIptr>
 {
-    public:
-    explicit DTP(Stream* bus, const shs::settings::shs_ModuleID_t ID);
-    ~DTP();
+public:
+    explicit DTP(Stream& bus, const shs::t::shs_ID_t ID)
+        : m_bus(bus), m_ID(ID), m_len(0), m_tmr(0)
+    {}
 
-    uint8_t tick();
-    uint8_t checkBus(uint8_t* len = nullptr);
+    ~DTP() = default;
 
-    uint8_t sendPacket(shs::ByteCollector* bc, const shs::settings::shs_ModuleID_t to);
-    uint8_t sendPacket(shs::ByteCollector* bc, const shs::settings::shs_ModuleID_t to, const shs::settings::shs_ID_t api_ID, const shs::settings::shs_ModuleID_t from);
 
-    private:
-    Stream* m_bus{};
-    uint8_t m_len{};
-    uint32_t m_tmr{};
-    shs::settings::shs_ModuleID_t m_ID{};
+    void start() override {}
+    void tick() override { checkBus(); }
+    void stop() override {}
+
+    uint8_t checkBus();
+    uint8_t sendPacket(shs::DTPpacket& packet) { return m_bus.write(packet.bc.getPtr(), packet.bc.size()); }
+
+    enum Status : uint8_t { no_data, packet_is_expected, packet_processed, invalid_recipient };
+
+private:
+    uint8_t m_handle_packet(shs::ByteCollector<>&& bc);
+
+    Stream& m_bus;
+    uint8_t m_len;
+    uint32_t m_tmr;
+    shs::t::shs_ID_t m_ID;
 };

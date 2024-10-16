bef10bd4eb28c5a0ae4a803476dd53ead2ef32f3
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-10-16 22:46:16 +0300

    Create DTP

---
+++ src/SHScore/shs_DTP.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+#include "shs_API.h"
+#include "shs_SortedBuf.h"
+#include "shs_algorithm.h"
+#include "shs_DTPbus.h"
+#include "shs_DTPpacket.h"
+
+namespace shs
+{
+    class DTP;
+    namespace shs::DTPless
+    {
+        struct APIptr;
+        struct APIid;
+    }
+}
+
+struct shs::DTPless::APIptr { bool operator()(const shs::API* lhs, const shs::API* rhs) const { return lhs->API_ID < rhs->API_ID; } };
+struct shs::DTPless::APIid
+{
+    bool operator()(const shs::API* lhs, const shs::t::shs_ID_t rhs) const { return lhs->API_ID < rhs; }
+    bool operator()(const shs::t::shs_ID_t lhs, const shs::API* rhs) const { return lhs < rhs->API_ID; }
+};
+
+
+class shs::DTP : public shs::API, public shs::SortedBuf<shs::API*>
+{
+public:
+
+    void handle(shs::ByteCollectorReadIterator<>& it) override;
+
+
+private:
+}

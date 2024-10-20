01d531ff29151f2ee101326aced39e1a2d0aed8d
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-10-20 20:15:58 +0300

    Update compilation_test

--- src/debugging_sketches/compilation_test/compilation_test.ino
+++ src/debugging_sketches/compilation_test/compilation_test.ino
@@ -1,21 +1,28 @@
-#include <shs_API.h>
-#include <shs_algorithm.h>
-#include <shs_ByteCollector.h>
-#include <shs_ControlWiFi.h>
-#include <shs_CRC.h>
-#include <shs_ID.h>
-#include <shs_Process.h>
 #include <shs_settings.h>
+
+#include <shs_algorithm.h>
+
+#include <shs_API.h>
+
+#include <shs_ByteCollector.h>
+#include <shs_ByteCollectorIterator.h>
+
+#include <shs_ControlWiFi.h>
+
+#include <shs_CRC.h>
+
+#include <shs_CreateProcess.h>
+#include <shs_Process.h>
+
+#include <shs_DTP.h>
+#include <shs_DTPbus.h>
+#include <shs_DTPpacket.h>
+
+#include <shs_ID.h>
+
+
 #include <shs_types.h>
 
-void setup() 
-{
 
-}
-
-
-void loop() 
-{
-
-}
-
+void setup() {}
+void loop() {}

a229443f7b7da87241e51b5a90986e6fdb8f2850
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-05 20:02:10 +0300

    Added TCP socket

--- src/SmartModules [beta]/smartRGB/settings.h
+++ src/SmartModules [beta]/smartRGB/settings.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <shs_settings.h>
 
 #define BOT_ACTIVE 1
-#define BOT_TOCKEN "5993724594:AAHW-eBzSQs2QktURq0Waolu6zuO0VMGoe0" // https://api.telegram.org/bot5993724594:AAHW-eBzSQs2QktURq0Waolu6zuO0VMGoe0/getUpdates
+#define BOT_TOCKEN shs::settings::SHSrgb_bot_TOKEN
+
+inline constexpr auto SERVER_IP = "192.168.1.10";
+inline constexpr auto PORT = 10002;
+
+inline constexpr uint8_t THIS_ID = 14; 
 
 #define BOT_NAME "@SHSrgb_bot"
 #define CHAT_ID "438901860"       // commander's chat
 #define GROUP_ID "-1001977013443" // smart home test
+#define S_IP 10
 
 #define Rp 14 // D5
 #define Gp 12 // D6
@@ -15,8 +22,8 @@
 // настройки пламени
 #define HUE_START 0    // начальный цвет огня (0 красный, 80 зелёный, 140 молния, 190 розовый)
 #define HUE_GAP 10     // коэффициент цвета огня (чем больше - тем дальше заброс по цвету)
-#define SMOOTH_K 0.10  // коэффициент плавности огня
-#define MIN_BRIGHT 80  // мин. яркость огня
+#define SMOOTH_K 0.15  // коэффициент плавности огня
+#define MIN_BRIGHT 100  // мин. яркость огня
 #define MAX_BRIGHT 255 // макс. яркость огня
 #define MIN_SAT 255    // мин. насыщенность
 #define MAX_SAT 255    // макс. насыщенность
--- src/SmartModules [beta]/smartRGB/smartRGB.ino
+++ src/SmartModules [beta]/smartRGB/smartRGB.ino
@@ -1,26 +1,43 @@
 #include "settings.h"
 
 
-#include <SHSlibrary.h>
+#include <shs_ControlWiFi.h>
 
 #include <FastBot.h>
 FastBot bot(BOT_TOCKEN);
 
 #include <GRGB.h>
-GRGB strip(COMMON_ANODE, Rp, Gp, Bp);
+GRGB strip(COMMON_CATHODE, Rp, Gp, Bp);
 
+#include <shs_DTP.h>
+#include <shsL_GRGB_API.h>
+#include <shs_ByteCollector.h>
+#include <shs_ByteCollectorIterator.h>
+#include <ESP8266WiFi.h>
+#include <shs_TcpClient.h>
+
+shs::TcpClient client(S_IP, PORT, THIS_ID);
+shs::GRGB_API grgb_api(strip, THIS_ID);
+shs::DTP dtp(client, grgb_api, THIS_ID);
 
 void setup() {
-  connectWiFi();
+  shs::ControlWiFi::connectWiFiWait();
+
   botSetup();
+
   analogWriteResolution(8);
+
+  //analogWriteFreq(10000);
+
+  //pinMode(D4, OUTPUT);
 }
 
 
 void loop() {
   bot.tick();
   RGB();
-  
+  strip.tick();
+
+  if (client.connected()) dtp.tick();
+  else if (client.connect(SERVER_IP, PORT)) client.write(&THIS_ID - 1, 2);
 }
-
-

c0a11fc7a4835abb0fa260d7fd7c909afbc32e77
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-11 23:21:35 +0300

    Not needed

--- src/create.sh
+++
@@ -1,7 +0,0 @@
-ArduinoLibraries=$(cat libraries_path)
-
-echo "ArduinoLibraries directory is: $ArduinoLibraries"
-
-mkdir "${ArduinoLibraries}SHScore/"
-mkdir "${ArduinoLibraries}SHSlibrary"
-mkdir "${ArduinoLibraries}SHStests"

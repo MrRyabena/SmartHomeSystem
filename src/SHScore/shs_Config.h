6db1fb541f2ce5a54342987831dadbf7a7615a88
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-12 20:39:22 +0300

    Returned from v1.0.0

---
+++ src/SHScore/shs_Config.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/*
+  Last update: v1.0.0
+  Versions:
+    v0.2.0 — created.
+    v1.0.0 — release.
+*/
+
+/*
+  A structure for storing module parameters.
+*/
+
+
+#include "shs_types.h"
+
+#include <stdint.h>
+
+namespace shs
+{
+    enum class SystemMode : uint8_t;
+    struct Config;
+};
+
+struct shs::Config
+{
+    static constexpr shs::t::shs_ID_t moduleID;
+    shs::SystemMode mode{};
+};
+
+enum class shs::SystemMode : uint8_t
+{
+    off,
+    on,
+    standby,
+    sleep,
+};

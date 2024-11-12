3f0183aa0262eef53d1e04cb7973df11a46e03c7
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-12 23:21:13 +0300

    Expanded

--- src/SHScore/shs_ID.h
+++ src/SHScore/shs_ID.h
@@ -8,6 +8,7 @@
 */
 
 #include <stdint.h>
+#include <memory>
 
 namespace shs
 {
@@ -18,35 +19,41 @@ struct shs::ID
 {
     uint32_t id{};
 
-    ID(const uint8_t module = 0, const uint8_t device = 0, const int16_t api = 0)
-        : id(((uint32_t)module << 24) | ((uint32_t)device << 16) | (api & 0xffff)) {}
+
+    constexpr explicit ID(const uint8_t module_id = 0, const uint8_t device_id = 0, const uint16_t api_id = 0)
+        : id(((uint32_t)module_id << 24) | ((uint32_t)device_id << 16) | (api_id & 0xffff))
+    {}
+
+    ID(const uint8_t module_id = 0, const uint8_t device_id = 0, const int16_t api_id = 0)
+        : id(((uint32_t)module_id << 24) | ((uint32_t)device_id << 16) | (api_id & 0xffff))
+    {}
 
     ~ID() = default;
 
-    uint8_t getModuleID() const { return id >> 24; }
-    uint8_t getDeviceID() const { return (id >> 16) & 0xff; }
-    int16_t getAPI() const { return id & 0xffff; }
+    constexpr uint8_t getModuleID() const { return id >> 24; }
+    constexpr uint8_t getDeviceID() const { return (id >> 16) & 0xff; }
+    constexpr uint16_t getApiID() const { return id & 0xffff; }
 
-    shs::ID &setModuleID(const uint8_t module)
+    shs::ID& setModuleID(const uint8_t module)
     {
         id |= (uint32_t)module << 24;
         return *this;
     }
 
-    shs::ID &setDeviceID(const uint8_t device)
+    shs::ID& setDeviceID(const uint8_t device)
     {
         id |= (uint32_t)device << 16;
         return *this;
     }
 
-    shs::ID &setAPI(const int16_t api)
+    shs::ID& setApiID(const uint16_t api)
     {
         id |= api & 0xffff;
         return *this;
     }
 
-    bool operator<(const shs::ID &other) const { return id < other.id; }
-    bool operator>(const shs::ID &other) const { return id > other.id; }
-    bool operator==(const shs::ID &other) const { return id == other.id; }
-    bool operator!=(const shs::ID &other) const { return id != other.id; }
+    bool operator<(const shs::ID& other) const { return id < other.id; }
+    bool operator>(const shs::ID& other) const { return id > other.id; }
+    bool operator==(const shs::ID& other) const { return id == other.id; }
+    bool operator!=(const shs::ID& other) const { return id != other.id; }
 };

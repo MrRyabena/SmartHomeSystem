11c17cc13e028ba1820d01d9152e3a8bea213257
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-14 14:41:05 +0300

    Added the bus->tick() call

--- src/SHScore/shs_DTP.cpp
+++ src/SHScore/shs_DTP.cpp
@@ -5,29 +5,31 @@ void shs::DTP::tick()
 {
     for (auto& bus : m_buss)
     {
-        if (bus->checkBus() == shs::DTPbus::packet_processed)
+        auto status = bus->checkBus();
+        if (status == shs::DTPbus::packet_processed)
         {
             auto it = bus->getLastData();
             switch (shs::DTPpacket::get_DTPcode(it))
             {
-            case shs::DTPpacket::STANDARD:
-            {
-                auto id = shs::DTPpacket::get_recipientID(it);
-                auto api = m_APIs.get(id);
+                case shs::DTPpacket::STANDARD:
+                    {
+                        auto id = shs::DTPpacket::get_recipientID(it);
+                        auto api = m_APIs.get(id);
 
-                if (api != m_APIs.end())
-                {
-                    auto output = std::move((*api)->handle(it));
-                    if (!output.empty()) bus->sendPacket(output);
-                }
-            }
-            break;
+                        if (api != m_APIs.end())
+                        {
+                            auto output = std::move((*api)->handle(it));
+                            if (!output.empty()) bus->sendPacket(output);
+                        }
+                    }
+                    break;
 
-            default: [[fallthrough]]
-            case shs::DTPpacket::FAST:
-                break;
+                default: [[fallthrough]]
+                case shs::DTPpacket::FAST:
+                    break;
 
             }
         }
+        bus->tick();
     }
 }

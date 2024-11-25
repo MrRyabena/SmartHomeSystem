79b202ac1696c24d0238628c2c6a8989203b68a6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-25 22:36:55 +0300

    Corrected with the changes in DTPbus

--- src/SHScore/shs_TcpServer.cpp
+++ src/SHScore/shs_TcpServer.cpp
@@ -8,16 +8,21 @@ void shs::TcpServer::tick()
         if (server.hasClientData())
         {
             WiFiClient client = server.available();
-            uint8_t len = client.read();
-            while (client.available() < len - 1);
-            shs::ByteCollector<> bc(len);
-            bc.push_back(len, 1);
-            for (uint8_t i = 0; i < len - 1; i++) bc.push_back(client.read(), 1);
+
+            uint8_t len{};
+            shs::DTPbus::Status status{};
+            shs::ByteCollector<> bc(10);
+
+
+            do status = shs::DTPbus::checkBus(client, bc, len);
+            while (status == shs::DTPbus::packet_is_expected); // need timer or async
+
+            if (status != shs::DTPbus::packet_received) return;
 
             shs::DTPpacket packet(std::move(bc));
             if (packet.get_DTPcode() == shs::DTPpacket::DTPcode::INITIAL)
             {
-                auto mes = shs::DTP_APIpackets::getInitialPacket(10);
+                auto mes = shs::DTP_APIpackets::getInitialPacket(10);  // need correct id
                 client.write(mes.bc.getPtr(), mes.bc.size());
                 auto busID = packet.get_senderID();
                 m_dtp.attachBus(std::make_unique<shs::TcpSocket>(client, busID, nullptr, 25, [](shs::TcpSocket&) {doutln("connect!");}, [this](shs::TcpSocket& socket) { doutln("disconnect!"); m_dtp.detachBus(socket.busID); }));

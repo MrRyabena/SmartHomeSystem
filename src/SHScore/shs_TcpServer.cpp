030a52ba12a3323fbf03b3b97272d873238ca3e7
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-11-27 01:17:38 +0300

    Processing the client while waiting for connection

--- src/SHScore/shs_TcpServer.cpp
+++ src/SHScore/shs_TcpServer.cpp
@@ -3,30 +3,33 @@
 
 void shs::TcpServer::tick()
 {
+    if (m_connecting_client)
+    {
+        if (m_connecting_client_time - shs::ProgramTime::s_microseconds() > max_connection_time)
+        {
+            m_connecting_client.reset();
+            return;
+        }
+
+        if (m_connecting_client.checkBus() != shs::DTPbus::packet_received) return;
+
+        if (shs::DTPpacket::get_DTPcode(m_connecting_client.getLastData()) == shs::DTPpacket::DTPcode::INITIAL)
+        {
+            // need send good callback
+            m_dtp.attachBus(std::move(m_connecting_client));
+        }
+        else
+        {
+            // need send error callback
+        }
+
+    }
+
     if (server.hasClient())
     {
-        if (server.hasClientData())
-        {
-            WiFiClient client = server.available();
-
-            uint8_t len{};
-            shs::DTPbus::Status status{};
-            shs::ByteCollector<> bc(10);
-
-
-            do status = shs::DTPbus::checkBus(client, bc, len);
-            while (status == shs::DTPbus::packet_is_expected); // need timer or async
-
-            if (status != shs::DTPbus::packet_received) return;
-
-            shs::DTPpacket packet(std::move(bc));
-            if (packet.get_DTPcode() == shs::DTPpacket::DTPcode::INITIAL)
-            {
-                auto mes = shs::DTP_APIpackets::getInitialPacket(10);  // need correct id
-                client.write(mes.bc.getPtr(), mes.bc.size());
-                auto busID = packet.get_senderID();
-                m_dtp.attachBus(std::make_unique<shs::TcpSocket>(client, busID, nullptr, 25, [](shs::TcpSocket&) {doutln("connect!");}, [this](shs::TcpSocket& socket) { doutln("disconnect!"); m_dtp.detachBus(socket.busID); }));
-            }
-        }
+        m_connecting_client = std::make_unique<shs::TcpSocket>(server.available(), 0, nullptr, 25, [](shs::TcpSocket&) {doutln("connect!");}, [this](shs::TcpSocket& socket) { doutln("disconnect!"); m_dtp.detachBus(socket.busID); });
+        m_connecting_client_time = shs::ProgramTime::s_microseconds();
+        auto mes = shs::DTP_APIpackets::getInitialPacket(10);  // need correct id
+        client.write(mes.bc.getPtr(), mes.bc.size());
     }
 }

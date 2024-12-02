f97a0c0be8c1931d6804ebc780554887a35da7cc
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-12-02 19:22:30 +0300

    The processing and initialization of connecting clients has been completed

--- src/SHScore/shs_TcpServer.cpp
+++ src/SHScore/shs_TcpServer.cpp
@@ -3,33 +3,40 @@
 
 void shs::TcpServer::tick()
 {
-    if (m_connecting_client)
+    if (m_connecting_client && m_connecting_client->client.connected())
     {
-        if (m_connecting_client_time - shs::ProgramTime::s_microseconds() > max_connection_time)
+        if (shs::ProgramTime::s_milliseconds() - m_connecting_client_time > max_connection_time)
         {
-            m_connecting_client.reset();
+            m_connecting_client->stop();
             return;
         }
 
-        if (m_connecting_client.checkBus() != shs::DTPbus::packet_received) return;
 
-        if (shs::DTPpacket::get_DTPcode(m_connecting_client.getLastData()) == shs::DTPpacket::DTPcode::INITIAL)
+        if (m_connecting_client->checkBus() != shs::DTPbus::packet_received && m_connecting_client->status != shs::DTPbus::packet_processed) return;
+
+
+        if (shs::DTPpacket::get_DTPcode(m_connecting_client->getLastData()) == shs::DTPpacket::DTPcode::INITIAL_ANSWER)
         {
-            // need send good callback
+            auto answer = shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, true);
+
+            m_connecting_client->sendPacket(answer);
+
             m_dtp.attachBus(std::move(m_connecting_client));
         }
         else
         {
-            // need send error callback
+            m_connecting_client->sendPacket(shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, false));
+            m_connecting_client->stop();
         }
-
     }
 
     if (server.hasClient())
     {
-        m_connecting_client = std::make_unique<shs::TcpSocket>(server.available(), 0, nullptr, 25, [](shs::TcpSocket&) {doutln("connect!");}, [this](shs::TcpSocket& socket) { doutln("disconnect!"); m_dtp.detachBus(socket.busID); });
-        m_connecting_client_time = shs::ProgramTime::s_microseconds();
-        auto mes = shs::DTP_APIpackets::getInitialPacket(10);  // need correct id
-        client.write(mes.bc.getPtr(), mes.bc.size());
+        m_connecting_client = std::make_unique<shs::TcpSocket>(server.available(), 0, nullptr, 25, nullptr, [this](shs::TcpSocket& socket) { m_dtp.detachBus(socket.busID); });
+
+        m_connecting_client_time = shs::ProgramTime::s_milliseconds();
+
+        auto mes = shs::DTP_APIpackets::getInitialPacket(m_dtp.moduleID);
+        m_connecting_client->sendPacket(mes);
     }
 }

3b16876cfe84734f5726d9ac0d1fff6d706218a7
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-12-02 19:19:11 +0300

    The handler has not been written yet

--- src/SHScore/shs_DTP_API.cpp
+++ src/SHScore/shs_DTP_API.cpp
@@ -6,6 +6,7 @@ shs::DTPpacket shs::DTP_APIhandler::handle(shs::ByteCollectorReadIterator<>& it)
     switch (shs::DTPpacket::get_DTPcode(it))
     {
         case shs::DTPpacket::INITIAL: break;
+        case shs::DTPpacket::INITIAL_ANSWER: break;
         default: break;
     }
 

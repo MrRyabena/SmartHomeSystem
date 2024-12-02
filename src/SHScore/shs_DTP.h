448b17d060bb2501bb0871edb831b2bce3ccfe10
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-12-02 19:16:33 +0300

    The work with BusID has been redone

--- src/SHScore/shs_DTP.h
+++ src/SHScore/shs_DTP.h
@@ -1,5 +1,30 @@
 #pragma once
 
+/*
+  Smart Home System Data Transmission Protocol
+
+  The idea is taken from https://github.com/GyverLibs/GyverBus
+*/
+
+/*
+  Last update: v1.0.0
+  Versions:
+    v0.1.0 — created.
+    v0.2.0
+      - Divided into two classes.
+      - A new type of handlers.
+      - Added inheritance from class shs::CallbacksKeeper.
+      - Using a new class for CRC.
+    v1.0.0 — release.
+      - Namespaces added.
+      - Variable names have been corrected.
+    v2.0.0 — global changes
+      - 
+*/
+
+
+#include <memory>
+
 
 #include "shs_Process.h"
 #include "shs_API.h"
@@ -9,7 +34,6 @@
 #include "shs_DTPbus.h"
 #include "shs_DTPpacket.h"
 
-#include <memory>
 
 namespace shs
 {
@@ -41,11 +65,11 @@ struct shs::DTPless::BUS
     bool operator()(const shs::DTPbus* lhs, const shs::DTPbus* rhs) const { return *lhs < *rhs; }
     bool operator()(const std::unique_ptr<shs::DTPbus>& lhs, const std::unique_ptr<shs::DTPbus>& rhs) const { return *lhs < *rhs; }
 
-    bool operator()(const shs::DTPbus* lhs, const shs::t::shs_ID_t rhs) const { return lhs->busID < rhs; }
-    bool operator()(const shs::t::shs_ID_t lhs, const shs::DTPbus* rhs) const { return lhs < rhs->busID; }
+    bool operator()(const shs::DTPbus* lhs, const shs::t::shs_busID_t rhs) const { return lhs->busID < rhs; }
+    bool operator()(const shs::t::shs_busID_t lhs, const shs::DTPbus* rhs) const { return lhs < rhs->busID; }
 
-    bool operator()(const std::unique_ptr<shs::DTPbus>& lhs, const shs::t::shs_ID_t rhs) const { return lhs->busID < rhs; }
-    bool operator()(const shs::t::shs_ID_t lhs, const std::unique_ptr<shs::DTPbus>& rhs) const { return lhs < rhs->busID; }
+    bool operator()(const std::unique_ptr<shs::DTPbus>& lhs, const shs::t::shs_busID_t rhs) const { return lhs->busID < rhs; }
+    bool operator()(const shs::t::shs_busID_t lhs, const std::unique_ptr<shs::DTPbus>& rhs) const { return lhs < rhs->busID; }
 };
 
 
@@ -53,18 +77,36 @@ class shs::DTP : public shs::Process
 {
 public:
 
+    shs::t::shs_ID_t moduleID;
+
+
+    explicit DTP(const shs::t::shs_ID_t module_id) : moduleID(module_id) {}
+
+
+    // shs::Process
     void start() override { for (auto& bus : m_buss) bus->start(); }
     void tick() override;
     void stop() override { for (auto& bus : m_buss) bus->stop(); }
 
-    void attachBus(std::unique_ptr<shs::DTPbus>&& bus) { m_buss.attach(std::move(bus)); }
-    void detachBus(const shs::t::shs_ID_t& id) { m_buss.detach(id); }
-    std::unique_ptr<shs::DTPbus>& getBus(const shs::t::shs_ID_t& id) { return *m_buss.get(id); }
 
+    // BUS
+    shs::t::shs_busID_t attachBus(std::unique_ptr<shs::DTPbus>&& bus)
+    {
+
+        if (bus->busID == 0 || m_buss.get(bus) != m_buss.end()) bus->busID = getUniqueBusID();
+        return (*m_buss.attach(std::move(bus)))->busID;
+    }
+
+    void detachBus(const shs::t::shs_busID_t& id) { m_buss.detach(id); }
+    shs::DTPbus& getBus(const shs::t::shs_busID_t& id) const { return *(m_buss.get(id)->get()); }
+
+    shs::t::shs_busID_t getUniqueBusID() const;
+
+
+    // API
     void attachAPI(std::unique_ptr<shs::API>&& api) { m_APIs.attach(std::move(api)); }
     void detachAPI(const shs::t::shs_ID_t& id) { m_APIs.detach(id); }
-    std::unique_ptr<shs::API>& getAPI(const shs::t::shs_ID_t& id) { return *m_APIs.get(id); }
-
+    shs::API& getAPI(const shs::t::shs_ID_t& id) const { return *(m_APIs.get(id)->get()); }
 
 private:
     shs::SortedBuf<std::unique_ptr<shs::DTPbus>, DTPless::BUS> m_buss;

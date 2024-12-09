61023de28610684f0f30f2b0490b1867b854ce16
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-12-09 20:25:36 +0300

    Added new types

--- src/SHScore/shs_types.h
+++ src/SHScore/shs_types.h
@@ -12,11 +12,23 @@
 
 #include <stdint.h>
 
+
 #ifdef SHS_SF_ARDUINO
 #include <Arduino.h>
+
+#ifdef SHS_SF_ESP8266
+
+#include <WiFiUdp.h>
+
+#elif defined(SHS_SF_ESP32)
+#include <WiFiUdp.h>
+#elif defined(SHS_SF_AVR)
+#endif
+
 #else
 #include <string>
 #endif
+
 
 namespace shs
 {
@@ -30,12 +42,18 @@ namespace shs
 
     #ifdef SHS_SF_ARDUINO
         using shs_string_t = String;
+
+        using shs_IP_t = IPAddress;
     #else
         using shs_string_t = std::string;
+
+        using shs_IP_t = uint32_t;
     #endif
 
         using shs_float_t = float;
         using shs_ID_t = shs::ID;
         using shs_busID_t = uint16_t;
+
+        using shs_port_t = uint16_t;
     }
 }

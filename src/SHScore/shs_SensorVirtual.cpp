586085feb564aacebf98edc28faefaca2652043b
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-01-12 17:45:00 +0300

    Created and realized

---
+++ src/SHScore/shs_SensorVirtual.cpp
@@ -0,0 +1,47 @@
+#include "shs_SensorVirtual.h"
+
+
+
+shs::DTPpacket shs::SensorVirtual::handle(shs::ByteCollectorReadIterator<>& it)
+{
+    it.set_position(shs::DTPpacket::get_dataBeg(it));
+
+    switch (it.read())
+    {
+        case shs::Sensor_API::Commands::DATA:
+            it.read(m_value);
+            m_updatedFlag = true;
+            break;
+
+        default:
+            break;
+    }
+
+    return shs::DTPpacket();
+}
+
+
+void shs::SensorVirtual::update()
+{
+    shs::ByteCollector<> bc(2);
+    bc.push_back(shs::Sensor_API::Commands::requestData, 1);
+    bc.push_back(false, 1);
+
+    m_dtp.sendPacket(shs::DTPpacket(API_ID, m_virtualID, std::move(bc)));
+
+    m_updatedFlag = false;
+}
+
+
+void shs::SensorVirtual::updateFast()
+{
+    shs::ByteCollector<> bc(2);
+    bc.push_back(shs::Sensor_API::Commands::requestData, 1);
+    bc.push_back(true, 1);
+
+    m_dtp.sendPacket(shs::DTPpacket(API_ID, m_virtualID, std::move(bc)));
+
+    m_updatedFlag = false;
+}
+
+
---
+++ src/SHScore/shs_SensorVirtual.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <memory>
+
+#include "shs_Sensor.h"
+#include "shs_Sensor_API.h"
+#include "shs_types.h"
+#include "shs_API.h"
+#include "shs_ByteCollector.h"
+#include "shs_ByteCollectorIterator.h"
+#include "shs_DTP.h"
+#include "shs_DTPpacket.h"
+
+
+namespace shs
+{
+    class SensorVirtual;
+}
+
+
+class shs::SensorVirtual : public shs::Sensor, public shs::API
+{
+public:
+    explicit SensorVirtual(const shs::t::shs_ID_t thisID, const shs::t::shs_ID_t virtualID, shs::DTP& dtp, const shs::Sensor::Type stype = UNKNOWN)
+        : API(id), Sensor(stype), m_virtualID(virtualID), m_dtp(dtp)
+    {}
+
+    ~SensorVirtual() = default;
+
+    // API
+    shs::DTPpacket handle(shs::ByteCollectorReadIterator<>& it) override;
+
+    // Sensor
+    void update() override;
+    void updateFast() override;
+
+    bool isUpdated() override { return m_updatedFlag; }
+
+    int32_t              getValueI() override { return static_cast<int32_t>(m_value); }
+    shs::t::shs_float_t  getValueF() override { return static_cast<shs::t::shs_float_t>(m_value); }
+    shs::t::shs_double_t getValueD() override { return static_cast<shs::t::shs_double_t>(m_value); }
+
+protected:
+    shs::t::shs_ID_t m_virtualID;
+    shs::DTP& m_dtp;
+
+    shs::t::shs_fixed_t m_value{};
+    bool m_updatedFlag{};
+};

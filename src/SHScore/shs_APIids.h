c2296f0368a5a2297563d7052539d465aed80223
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-01-16 00:07:42 +0300

    Renamed and redesigned

---
+++ src/SHScore/shs_APIids.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace shs
+{
+    namespace constants
+    {
+        enum class APIids : uint16_t;
+    }
+}
+
+
+enum class shs::constants::APIids : uint16_t
+{
+    NOID,
+
+    DTP = 0x01,
+    DTPbus,
+    DTPserver_bus,
+
+    Load,
+    Sensor,
+    EventTracker,
+
+
+    SHS_LIBRARY_IDs = 0x4000,
+    USER_IDs = 0x8000,
+
+};

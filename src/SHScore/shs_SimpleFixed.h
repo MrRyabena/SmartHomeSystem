1e3f8d7d9a39d181dc49811d3b1eb287694b02e0
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-01-19 01:06:18 +0300

    Added setters and assignment operators

--- src/SHScore/shs_SimpleFixed.h
+++ src/SHScore/shs_SimpleFixed.h
@@ -24,23 +24,33 @@ namespace shs
 class shs::SimpleFixed
 {
 public:
-    SimpleFixed(shs::t::shs_double_t value, uint8_t power_of_ten = 4) noexcept
-        : m_value(static_cast<int32_t>(value* std::pow(10, power_of_ten)) & 0xffffffe0 | power_of_ten & 1f)
+    static constexpr auto DEFAULT_POWER_OF_TEN = 4;
+
+    SimpleFixed(const double value = 0, const uint8_t power_of_ten = DEFAULT_POWER_OF_TEN) noexcept
+        : m_value(static_cast<int32_t>(value* std::pow(10, power_of_ten)) & 0xffffffe0 | power_of_ten & 0x1f)
     {}
 
-    shs::t::shs_float_t toFloat() const { return static_cast<shs::t::shs_float_t>(m_value & 0xffffffe0) * m_value & 1f; }
-    shs::t::shs_double_t toDouble() const { return static_cast<shs::t::shs_double_t>(m_value & 0xffffffe0) * m_value & 1f; }
+    float toFloat()   const { return static_cast<float>(m_value & 0xffffffe0) * (m_value & 0x1f); }
+    double toDouble() const { return static_cast<double>(m_value & 0xffffffe0) * (m_value & 0x1f); }
+
+    void set(const int32_t value, const uint8_t power_of_ten = DEFAULT_POWER_OF_TEN) { m_value = value << 5; }
+    void set(const float   value, const uint8_t power_of_ten = DEFAULT_POWER_OF_TEN) { m_value = static_cast<int32_t>(value * std::pow(10, power_of_ten)) & 0xffffffe0 | power_of_ten & 0x1f; }
+    void set(const double  value, const uint8_t power_of_ten = DEFAULT_POWER_OF_TEN) { m_value = static_cast<int32_t>(value * std::pow(10, power_of_ten)) & 0xffffffe0 | power_of_ten & 0x1f; }
+
+    SimpleFixed& operator=(int32_t value) { set(value); return *this; }
+    SimpleFixed& operator=(float value) { set(value); return *this; }
+    SimpleFixed& operator=(double value) { set(value); return *this; }
+
+    operator float()    const { return toFloat(); }
+    operator double()   const { return toDouble(); }
+    operator int32_t()  const { return static_cast<int32_t>(toFloat()); }
+    operator int16_t()  const { return static_cast<int16_t>(toFloat()); }
+    operator int8_t()   const { return static_cast<int8_t>(toFloat()); }
+    operator uint32_t() const { return static_cast<uint32_t>(toFloat()); }
+    operator uint16_t() const { return static_cast<uint16_t>(toFloat()); }
+    operator uint8_t()  const { return static_cast<uint8_t>(toFloat()); }
 
 
-    operator int()      noexcept const { return static_cast<int>(toFloat()); }
-    operator float()    noexcept const { return static_cast<float>(toFloat()); }
-    operator double()   noexcept const { return toDouble(); }
-    operator int32_t()  noexcept const { return static_cast<int32_t>(toFloat()); }
-    operator int16_t()  noexcept const { return static_cast<int16_t>(toFloat()); }
-    operator int8_t()   noexcept const { return static_cast<int8_t>(toFloat()); }
-    operator uint32_t() noexcept const { return static_cast<uint32_t>(toFloat()); }
-    operator uint16_t() noexcept const { return static_cast<uint16_t>(toFloat()); }
-    operator uint8_t()  noexcept const { return static_cast<uint8_t>(toFloat()); }
 
 private:
     int32_t m_value;

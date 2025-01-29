3f4eebd284b04718665f81cf438b453a66f9908a
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-01-29 20:53:19 +0300

    The hardware part has been written

--- src/SHScore/shs_debug.h
+++ src/SHScore/shs_debug.h
@@ -9,14 +9,16 @@
 #include "shs_settings_private.h"
 #ifdef DEBUG
 
-#if __has_include(<Arduino.h>)
+#ifdef SHS_SF_ARDUINO
 #include <Arduino.h>
+#define dinit() Serial.begin(115200);
 #define dout(value) Serial.print(value);
 #define doutln(value) Serial.println(value);
 #define dsep() Serial.println("----------------------------------------");
 
 #else
 #include <iostream>
+#define dinit()
 #define dout(value) std::cout << value;
 #define doutln(value) std::cout << value << std::endl;
 #define dsep() std::cout << "----------------------------------------" << std::endl;
@@ -24,7 +26,7 @@
 #endif
 
 #else
-
+#define dinit
 #define dout(value)
 #define doutln(value)
 #define dsep()

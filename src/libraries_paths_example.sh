0eb16be3e339987b3a9f40b15b407044d8885898
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-03 19:03:16 +0300

    Added examples of folder paths

--- src/libraries_paths_example.sh
+++ src/libraries_paths_example.sh
@@ -1,1 +1,11 @@
-/mnt/c/your/path/to/Arduino/libraries/
+ARDUINO_PATHS=(
+    "/mnt/c/your/path/to/Arduino/libraries/"
+)
+
+QT_PATHS=(
+    "./SHSapp [beta]/SHSapp_development/AppDev/SHSbuild"
+)
+
+OTHER_PATHS=(
+    "/other/paths"
+)

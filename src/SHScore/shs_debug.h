98080a9d5b397c77875984ebbf08d3011bd2fd5a
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-09 13:04:16 +0300

    Added a space in dout(), changed the debug-flag

--- src/SHScore/shs_debug.h
+++ src/SHScore/shs_debug.h
@@ -1,28 +1,23 @@
 #pragma once
 
 /*
-  Last update: v1.1.0
+  Last update: v2.0.0
   Versions:
     v1.1.0 — created.
+    v2.0.0 — Added a space in dout(), changed the debug-flag
 */
 
 #include "shs_settings_private.h"
-#ifdef DEBUG
+
+
+#ifdef SHS_SF_DEBUG
 
 #ifdef SHS_SF_ARDUINO
 #include <Arduino.h>
 #define dinit() Serial.begin(115200);
-#define dout(value) (Serial.print(value); Serial.print(' ');)
+#define dout(value) ({Serial.print(value); Serial.print(' ');})
 #define doutln(value) Serial.println(value);
 #define dsep() Serial.println("----------------------------------------");
-
-#elif defined(SHS_SF_QT)
-#include <QDebug>
-#include <iostream>
-#define dinit()
-#define dout(value) std::cout << value << ' ';
-#define doutln(value) qDebug() << value;
-#define dsep() qDebug() << "----------------------------------------";
 
 
 #else

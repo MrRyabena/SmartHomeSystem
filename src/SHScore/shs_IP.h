ee444918c1a9f6403e8cf88b0f9785fd6f4943b8
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-09 13:24:45 +0300

    Bug with ESP fixed, flags changed

--- src/SHScore/shs_IP.h
+++ src/SHScore/shs_IP.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "shs_settings_private.h"
+#include "shs_types.h"
 
 #if defined(SHS_SF_ESP8266) || defined(SHS_SF_ESP32)
 #include <IPAddress.h>
@@ -20,6 +21,11 @@ struct shs::IP
     constexpr IP(const char* ipAddress) : m_IP(ipFromStr(ipAddress)) {}
     constexpr IP(const uint32_t ipAddress = 0) : m_IP(ipAddress) {}
 
+#ifdef SHS_SF_ESP
+    IP(const IPAddress& ipAddress) : m_IP(reverse_bytes(static_cast<uint32_t>(ipAddress))) {}
+    IP& operator=(const IPAddress& ipAddress) { m_IP = reverse_bytes(static_cast<uint32_t>(ipAddress)); return *this; }
+#endif
+
     constexpr IP& operator=(const char* ipAddress) { m_IP = ipFromStr(ipAddress); return *this; }
     constexpr IP& operator=(const uint32_t ipAddress) { m_IP = ipAddress; return *this; }
 
@@ -31,12 +37,15 @@ struct shs::IP
 
 
     static constexpr uint32_t ipFromStr(const char* m_IP);
+    // shs::t::shs_string_t toString() { return shs::t::shs_string_t(m_IP >> 24) + '.' + (m_IP >> 16) & 0xff + '.' + (m_IP >> 8) & 0xff + '.' + m_IP & 0xff; }
 
     operator uint32_t() const { return m_IP; }
 
-#if (defined(SHS_SF_ESP8266) || defined(SHS_SF_ESP32))
-    operator IPAddress() const { return IPAddress(m_IP); }
+#ifdef SHS_SF_ESP
+    operator IPAddress() const { return reverse_bytes(m_IP); }
 #endif
+
+    static constexpr uint32_t reverse_bytes(const uint32_t ip) { return ((ip >> 24) & 0xFF) | ((ip << 8) & 0xFF0000) | ((ip >> 8) & 0xFF00) | ((ip << 24) & 0xFF000000); }
 
 private:
     uint32_t m_IP;
@@ -51,7 +60,6 @@ constexpr uint32_t shs::IP::ipFromStr(const char* ip)
     size_t index = 0;
     return m_ipToUInt32(ip, index);
 }
-
 
 
 constexpr uint32_t shs::IP::m_parseOctet(const char* str, size_t& index)

66ce8f13c75438a4b8b831153441907f9f2bb5dd
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-09 13:26:19 +0300

    Load management in switching mode

---
+++ src/SHScore/shs_LoadSwitch.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "shs_settings_private.h"
+#include "shs_Load.h"
+
+
+#ifdef SHS_SF_ARDUINO
+
+#include <GyverIO.h>
+
+
+namespace shs
+{
+    class LoadSwitch;
+}
+
+
+class shs::LoadSwitch : public shs::Load
+{
+public:
+    explicit LoadSwitch(const uint8_t pin) : Load(Load::Type::SWITCH), m_pin(pin) {}
+    ~LoadSwitch() override = default;
+
+    void setup() override
+    {
+    #ifdef SHS_SF_ESP
+        gio::init(m_pin, OUTPUT);
+    #else
+        gio::mode(m_pin, OUTPUT);
+    #endif
+    }
+
+    void on(const uint16_t value = UINT16_MAX) override { gio::write(m_pin, value); m_value = static_cast<bool>(value); }
+    void off() override { gio::low(m_pin); m_value = 0; }
+
+protected:
+    const uint8_t m_pin;
+};
+
+#endif  // #ifdef SHS_SF_ARDUINO

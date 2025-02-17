d9846ed3277eeb0115c278ef931d53d05d6bcd07
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-17 23:18:11 +0300

    Make for qt (GUIapp)

--- src/examples/guide_system/make.sh
+++ src/examples/guide_system/make.sh
@@ -2,3 +2,10 @@ source ../../libraries_paths.sh
 
 mkdir -p "${ARDUINO_PATHS}SHSconfig/"
 cp -r gs_config.h "${ARDUINO_PATHS}SHSconfig/"
+
+
+MODULE_3_PATH="module_3/GUIapp/SHSbuild"
+mkdir -p "${MODULE_3_PATH}/SHSconfig"
+cp -r gs_config.h "${MODULE_3_PATH}/SHSconfig"
+cp -r "../../SHScore" ${MODULE_3_PATH}
+cp -r "../../SHSqt_core" ${MODULE_3_PATH}

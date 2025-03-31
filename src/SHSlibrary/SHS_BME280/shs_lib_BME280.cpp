f65f53a9b99ed7c889ba62c09a40ca4786220bf8
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-03-31 19:15:35 +0300

    The BME/BMP280 sensor class has been created.

---
+++ src/SHSlibrary/SHS_BME280/shs_lib_BME280.cpp
@@ -0,0 +1,34 @@
+#include "shs_lib_BME280.h"
+
+
+void shs::BME280::update()
+{
+    if (!m_data.hasData() || m_data.isExpired()) updateForced();
+}
+
+
+void shs::BME280::updateForced(bool fast)
+{
+    m_data.update({ bme.readPressure(), bme.readTemperature(), bme.readHumidity() });
+
+    if (m_data.hasData() == false) { m_status = Status::CACHE_ERROR; return; }
+    if (isnan(m_data->temperature) || isnan(m_data->humidity)) { m_status = Status::FAILED_READ; return; }
+    m_status = Status::OK;
+}
+
+
+shs::t::shs_float_t shs::BME280::getValueF(const uint8_t metric)
+{
+    if (m_status != Status::OK || m_data.hasData()) return 0;
+
+    switch (metric)
+    {
+        case shs::etoi(Metrics::PRESSURE_BAR): return m_data->pressure; break;
+        case shs::etoi(Metrics::PRESSURE_MmHg): return pressureToMmHg(m_data->pressure); break;
+        case shs::etoi(Metrics::HUMIDITY): return m_data->humidity; break;
+        case shs::etoi(Metrics::TEMPERATURE): return m_data->temperature; break;
+
+        default: return 0; break;
+    }
+    return 0;
+}
---
+++ src/SHSlibrary/SHS_BME280/shs_lib_BME280.h
@@ -0,0 +1,64 @@
+#pragma once
+
+
+#include <shs_Sensor.h>
+#include <shs_TimedData.h>
+#include <shs_lib_SensorTypes.h>
+
+#include <shs_types.h>
+#include <shs_utils.h>
+
+#include <stdint.h>
+
+
+#include <GyverBME280.h>
+
+
+namespace shs
+{
+    class BME280;
+}
+
+
+class shs::BME280 : public shs::Sensor
+{
+public:
+    GyverBME280 bme;
+
+    enum class Metrics : uint8_t { PRESSURE_BAR, PRESSURE_MmHg, HUMIDITY, TEMPERATURE };
+    enum class Status : uint8_t { OK, NO_CONNECTION, CACHE_ERROR, FAILED_READ };
+
+    explicit BME280(const uint32_t cache_expiration_time)
+        :
+        shs::Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::BME280)),
+        m_data(cache_expiration_time)
+    {}
+
+    ~BME280() override = default;
+
+    void setup() override { m_status = bme.begin() ? Status::OK : Status::NO_CONNECTION; }
+
+    void update() override;
+    void updateFast() override { update(); }
+    void updateForced(bool fast = false) override;
+
+    [[nodiscard]] bool isUpdated() override { return !m_data.isExpired(); }
+    [[nodiscard]] uint8_t getStatus() override { return shs::etoi(m_status); }
+
+    [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return static_cast<int32_t>(getValueF(metric)); }
+    [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return shs::t::shs_fixed_t(getValueF(metric)); }
+    [[nodiscard]] shs::t::shs_float_t  getValueF(const uint8_t metric = 0) override;
+    [[nodiscard]] shs::t::shs_double_t getValueD(const uint8_t metric = 0) override { return static_cast<shs::t::shs_double_t>(getValueF(metric)); }
+
+protected:
+    struct Data
+    {
+        shs::t::shs_float_t pressure{};
+        shs::t::shs_float_t temperature{};
+        shs::t::shs_float_t humidity{};
+    };
+
+    shs::TimedData<Data> m_data;
+
+    Status m_status{};
+};

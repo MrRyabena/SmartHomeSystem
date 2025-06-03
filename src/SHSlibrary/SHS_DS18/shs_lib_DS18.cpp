31ad7f9d2afe00dafcc3012dfda78ec257de91f5
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-06-03 15:00:40 +0300

    Completed, but need testing.

--- src/SHSlibrary/SHS_DS18/shs_lib_DS18.cpp
+++ src/SHSlibrary/SHS_DS18/shs_lib_DS18.cpp
@@ -1,31 +1,46 @@
 #include "shs_lib_DS18.h"
 
 
+
+shs::DS18::DS18(const shs::t::shs_pin_t pin, const bool parasite)
+    : Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::DS18)), gds(pin, parasite), m_data(5000)
+{}
+
+
 void shs::DS18::update()
 {
-    if (!m_data.hasData() || m_data.isExpired()) updateForced();
+    if (!isUpdated()) updateForced();   
 }
+
 
 void shs::DS18::updateForced(bool fast)
 {
-    m_data.update({ bme.readPressure(), bme.readTemperature(), bme.readHumidity() });
+    if (!gds.isWaiting() && m_data.isExpired()) gds.requestTemp();
+    m_status = Status::UPDATING;
+}
 
-    if (m_data.hasData() == false) { m_status = Status::CACHE_ERROR; return; }
-    if (is_nan(m_data->temperature) || is_nan(m_data->humidity)) { m_status = Status::FAILED_READ; return; }
-    m_status = Status::OK;
+
+bool shs::DS18::isUpdated()
+{
+    if (!gds.ready() || m_data.isExpired()) return false;
+
+    if (m_status == Status::UPDATING && gds.ready())
+    {
+        if (!gds.readTemp()) m_status = Status::FAILED_READ;
+        m_data.update(gds.getTemp());
+        
+        if (m_data.hasData() == false) { m_status = Status::CACHE_ERROR; return false; }
+        if (isnan(*m_data)) { m_status = Status::FAILED_READ; return false; }
+        m_status = Status::OK;
+    }
+
+    return true;
 }
 
 
 shs::t::shs_float_t shs::DS18::getValueF(const uint8_t metric)
 {
-    if (m_status != OK || m_data.hasData()) return 0;
-
-    switch (metric)
-    {
-        case shs::etoi(TEMPERATURE): return m_data->temperature; break;
-        case shs::etoi(HUMIDITY): return m_data->humidity break;
-
-        default: return 0; break;
-    }
-    return 0;
+    if (!m_data.hasData()) return 0; // m_status != Status::OK || 
+ 
+    return *m_data;
 }
--- src/SHSlibrary/SHS_DS18/shs_lib_DS18.h
+++ src/SHSlibrary/SHS_DS18/shs_lib_DS18.h
@@ -1,12 +1,21 @@
 #pragma once
 
+/*
+  Last update: v2.1.0
+  Versions:
+    v2.0.0 — development started.
+    v2.1.0 — completed, but need testing.
+*/
 
-#include <DS18.h>                         // https://github.com/GyverLibs/DS18
+#include <GyverDS18.h>                 // https://github.com/GyverLibs/GyverDS18
 
 #include <shs_Sensor.h>
 #include <shs_lib_SensorTypes.h>
 #include <shs_TimedData.h>
 #include <shs_types.h>
+
+#include <shs_lib_SensorTypes.h>
+#include <shs_utils.h>
 
 
 namespace shs
@@ -20,40 +29,34 @@ class shs::DS18 : public shs::Sensor
 public:
     GyverDS18Single gds;
 
-    enum class Status { OK, FAILED_READ, CACHE_ERROR };
-    enum class Metrics { TEMPERATURE, HUMIDITY }
+    enum class Status { OK, FAILED_READ, CACHE_ERROR, UPDATING };
+    enum class Metrics { TEMPERATURE };
 
-    explicit DS18(const uint8_t pin, const bool parasite = true) : Sensor(shs::Sensor::Type::DIGITAL), gds(pin, parasite) {}
+    explicit DS18(shs::t::shs_pin_t pin, bool parasite = false);
     ~DS18() override = default;
 
 
-    void setup() override { gds.request(); }
+    void setup() override { gds.requestTemp(); }
 
     void update();
     void updateFast() override { update(); }
     void updateForced(bool fast = false) override;
 
-    [[nodiscard]] virtual bool isUpdated() = 0;
-    [[nodiscard]] virtual uint8_t getStatus() = 0;
+    [[nodiscard]] bool isUpdated() override;
+    [[nodiscard]] uint8_t getStatus() { return shs::etoi(m_status); };
 
 
-    virtual void clearCache() = 0;
-    virtual void setCacheExpiration(const uint32_t expiration_time) = 0;
-    virtual uint32_t getCacheExpiration() const = 0;
+    void clearCache() override { m_data.freeExpired(); }
+    void setCacheExpiration(const uint32_t expiration_time) override { m_data.setExpiration(expiration_time); }
+    uint32_t getCacheExpiration() const override { return m_data.getExpiration(); }
 
 
-    [[nodiscard]] virtual int32_t              getValueI(const uint8_t metric = 0) = 0;
-    [[nodiscard]] virtual shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) = 0;
-    [[nodiscard]] virtual shs::t::shs_float_t  getValueF(const uint8_t metric = 0) = 0;
-    [[nodiscard]] virtual shs::t::shs_double_t getValueD(const uint8_t metric = 0) = 0;
+    [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return static_cast<int32_t>(getValueF()); }
+    [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return static_cast<shs::t::shs_fixed_t>(getValueF()); }
+    [[nodiscard]] shs::t::shs_float_t  getValueF(const uint8_t metric = 0) override;
+    [[nodiscard]] shs::t::shs_double_t getValueD(const uint8_t metric = 0) override { return static_cast<shs::t::shs_double_t>(getValueD()); }
 
 protected:
     shs::TimedData<shs::t::shs_float_t> m_data;
     Status m_status;
-
 };
-
-
-constexpr auto operator+(shs::GyverDS18_Sensor::Metrics value) noexcept { return static_cast<uint8_t>(value); }
-constexpr auto operator+(shs::MHZ19::Status value) noexcept { return static_cast<uint8_t>(value); }
-

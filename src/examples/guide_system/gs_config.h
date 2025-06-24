494efc43ccd8421964ddd2b20dbed826f475312b
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-06-25 01:19:28 +0300

    Updated module names.

--- src/examples/guide_system/gs_config.h
+++ src/examples/guide_system/gs_config.h
@@ -9,36 +9,36 @@ namespace shs
     {
         enum class ModuleIDs : shs::t::shs_moduleID_t;
 
-        struct Module_1;
-        struct Module_2;
-        struct Module_3;
+        struct Module_1_sensor;
+        struct Module_2_load;
+        struct Module_3_gui;
     }
 }
 
 
 enum class shs::config::ModuleIDs : shs::t::shs_moduleID_t
 {
-    MODULE_1 = 1,
-    MODULE_2 = 2,
-    MODULE_3 = 3
+    MODULE_1_SENSOR = 1,
+    MODULE_2_LOAD = 2,
+    MODULE_3_GUI = 3
 };
 
 
-struct shs::config::Module_1
+struct shs::config::Module_1_sensor
 {
-    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_1);
+    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_1_SENSOR);
     enum DevicesIDs : shs::t::shs_deviceID_t { THERM_SENSOR = 1 };
 };
 
 
-struct shs::config::Module_2
+struct shs::config::Module_2_load
 {
-    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_2);
+    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_2_LOAD);
     enum DevicesIDs : shs::t::shs_deviceID_t { LOAD = 1 };
 };
 
 
-struct shs::config::Module_3
+struct shs::config::Module_3_gui
 {
-    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_3);
+    static constexpr shs::t::shs_moduleID_t MODULE_ID = static_cast<shs::t::shs_moduleID_t>(ModuleIDs::MODULE_3_GUI);
 };

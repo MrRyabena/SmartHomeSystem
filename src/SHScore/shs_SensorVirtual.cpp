374f76b05c68d5ea73bbb7a745ca7b5fbdd2fb7a
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-07-01 02:26:49 +0300

    Bug fixed: updated according to changes in the shs base class::Sensor.

--- src/SHScore/shs_SensorVirtual.cpp
+++ src/SHScore/shs_SensorVirtual.cpp
@@ -1,6 +1,13 @@
 #include "shs_SensorVirtual.h"
 
 
+shs::SensorVirtual::SensorVirtual(shs::t::shs_ID_t thisID, shs::t::shs_ID_t virtualID, shs::DTP& dtp, const shs::Sensor::Type stype, const uint32_t expiration_time)
+    : Sensor(stype), API(thisID.setComponentID(shs::constants::APIids::Sensor)),
+    m_virtualID(virtualID.setComponentID(shs::constants::APIids::Sensor)), m_dtp(dtp),
+    m_data(expiration_time)
+{}
+
+
 
 shs::DTPpacket shs::SensorVirtual::handle(shs::ByteCollectorReadIterator<>& it)
 {
@@ -9,8 +16,11 @@ shs::DTPpacket shs::SensorVirtual::handle(shs::ByteCollectorReadIterator<>& it)
     switch (it.read())
     {
         case shs::Sensor_API::Commands::DATA:
-            it.get(m_value);
-            m_updatedFlag = true;
+            {
+                shs::t::shs_fixed_t data;
+                it.get(data);
+                m_data.update(data);
+            }
             break;
 
         default:
@@ -23,17 +33,17 @@ shs::DTPpacket shs::SensorVirtual::handle(shs::ByteCollectorReadIterator<>& it)
 
 void shs::SensorVirtual::update()
 {
-    shs::ByteCollector<> bc(2);
-    bc.push_back(shs::Sensor_API::Commands::REQUEST_DATA, 1);
-    bc.push_back(false, 1);
-
-    m_dtp.sendPacket(shs::DTPpacket(API_ID, m_virtualID, std::move(bc)));
-
-    m_updatedFlag = false;
+    if (!isUpdated()) updateForced();
 }
 
 
 void shs::SensorVirtual::updateFast()
+{
+    update();
+}
+
+
+void shs::SensorVirtual::updateForced([[maybe_unused]] const bool fast)
 {
     shs::ByteCollector<> bc(2);
     bc.push_back(shs::Sensor_API::Commands::REQUEST_DATA, 1);
@@ -41,7 +51,5 @@ void shs::SensorVirtual::updateFast()
 
     m_dtp.sendPacket(shs::DTPpacket(API_ID, m_virtualID, std::move(bc)));
 
-    m_updatedFlag = false;
+    m_data.reset();
 }
-
-
--- src/SHScore/shs_SensorVirtual.h
+++ src/SHScore/shs_SensorVirtual.h
@@ -1,11 +1,12 @@
 #pragma once
 
 /*
-  Last update: v2.0.0
+  Last update: v2.2.0
   Versions:
     v0.2.0 — created.
     v1.0.0 — release.
     v2.0.0 — moved to a separate file, redesigned, debugged and tested.
+    v2.2.0 — bug fixed: updated according to changes in the shs base class::Sensor.
 */
 
 /*
@@ -24,6 +25,7 @@
 #include "shs_ByteCollectorIterator.h"
 #include "shs_DTP.h"
 #include "shs_DTPpacket.h"
+#include "shs_TimedData.h"
 
 
 namespace shs
@@ -35,9 +37,7 @@ namespace shs
 class shs::SensorVirtual : public shs::Sensor, public shs::API
 {
 public:
-    explicit SensorVirtual(shs::t::shs_ID_t thisID, shs::t::shs_ID_t virtualID, shs::DTP& dtp, const shs::Sensor::Type stype = UNKNOWN)
-        : Sensor(stype), API(thisID.setComponentID(shs::constants::APIids::Sensor)), m_virtualID(virtualID.setComponentID(shs::constants::APIids::Sensor)), m_dtp(dtp)
-    {}
+    explicit SensorVirtual(shs::t::shs_ID_t thisID, shs::t::shs_ID_t virtualID, shs::DTP& dtp, shs::Sensor::Type stype = UNKNOWN, uint32_t expiration_time = 10'000);
 
     ~SensorVirtual() = default;
 
@@ -46,21 +46,27 @@ public:
 
     // Sensor
     void setup() override {}
+    
     void update() override;
     void updateFast() override;
+    void updateForced([[maybe_unused]] bool fast = false) override;
+    
 
-    [[nodiscard]] bool isUpdated() override { return m_updatedFlag; }
+    [[nodiscard]] bool isUpdated() override { return !m_data.isExpired(); }
     [[nodiscard]] uint8_t getStatus() override { return 0; }
 
-    [[nodiscard]] int32_t              getValueI([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<int32_t>(m_value); }
-    [[nodiscard]] shs::t::shs_fixed_t  getValueFx([[maybe_unused]] const uint8_t metric = 0) override { return m_value; }
-    [[nodiscard]] shs::t::shs_float_t  getValueF([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<shs::t::shs_float_t>(m_value); }
-    [[nodiscard]] shs::t::shs_double_t getValueD([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<shs::t::shs_double_t>(m_value); }
+    void clearCache() override { m_data.freeExpired(); }
+    void setCacheExpiration(const uint32_t expiration_time) { m_data.setExpiration(expiration_time); }
+    uint32_t getCacheExpiration() const override { return m_data.getExpiration(); }
+
+    [[nodiscard]] int32_t              getValueI([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<int32_t>(*m_data); }
+    [[nodiscard]] shs::t::shs_fixed_t  getValueFx([[maybe_unused]] const uint8_t metric = 0) override { return *m_data; }
+    [[nodiscard]] shs::t::shs_float_t  getValueF([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<shs::t::shs_float_t>(*m_data); }
+    [[nodiscard]] shs::t::shs_double_t getValueD([[maybe_unused]] const uint8_t metric = 0) override { return static_cast<shs::t::shs_double_t>(*m_data); }
 
 protected:
     shs::t::shs_ID_t m_virtualID;
     shs::DTP& m_dtp;
 
-    shs::t::shs_fixed_t m_value{};
-    bool m_updatedFlag{};
+    shs::TimedData<shs::t::shs_fixed_t> m_data;
 };

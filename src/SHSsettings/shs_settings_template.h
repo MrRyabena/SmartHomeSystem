73326868a05e5d58a1c1be834aaa1d3f95f83254
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-07-18 16:38:08 +0300

    Now all settings are set via define.

--- src/SHSsettings/shs_settings_template.h
+++ src/SHSsettings/shs_settings_template.h
@@ -1,23 +1,17 @@
 #pragma once
 
 
-namespace shs
-{
-    namespace settings
-    {
-   
-    // #define SHS_SET_SENSOR_AVERAGE_SAMPLES 20
+// #define SET_WIFI_SSID "Your_SSID"
+// #define SET_WIFI_PASSWORD "Your_PASSWORD"
 
-    // #define SHS_SET_DEFAULT_TCP_PORT 5000
-    // #define SHS_SET_DEFAULT_UDP_PORT 6000
+// #define USE_FLOAT_FOR_DOUBLE 
 
-    // #define SHS_SET_DEFAULT_MULTICAST_IP "224.0.0.4"
-    // #define SHS_SET_DEFAULT_BROADCAST_IP "192.168.1.255"
+// #define SHS_SET_SENSOR_AVERAGE_SAMPLES 20
 
-    // #define SHS_SET_GMT 0
+// #define SHS_SET_DEFAULT_TCP_PORT 5000
+// #define SHS_SET_DEFAULT_UDP_PORT 6000
 
+// #define SHS_SET_DEFAULT_MULTICAST_IP "224.0.0.4"
+// #define SHS_SET_DEFAULT_BROADCAST_IP "192.168.1.255"
 
-        static constexpr auto WIFI_SSID = "Your_SSID";
-        static constexpr auto WIFI_PASSWORD = "Your_PASSWORD";
-    }
-}
+// #define SHS_SET_GMT 0

c1890badc9388193cf57a98d98de61cf823c7eec
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-07-22 22:47:05 +0300

    Fixed critical bugs: corrected MODULE_3_PATH and added cp shs_settings.h.

--- src/examples/guide_system/make.sh
+++ src/examples/guide_system/make.sh
@@ -4,12 +4,13 @@ mkdir -p "${ARDUINO_PATHS}SHSconfig/"
 cp -r gs_config.h "${ARDUINO_PATHS}SHSconfig/"
 
 
-MODULE_3_PATH="module_3/GUIapp/SHSbuild"
+MODULE_3_PATH="module_3_gui/GUIapp/SHSbuild"
 
 mkdir -p "${MODULE_3_PATH}/SHSconfig"
 cp -r gs_config.h "${MODULE_3_PATH}/SHSconfig"
 cp -r "../../SHScore" ${MODULE_3_PATH}
 cp -r "../../SHSqt_core" ${MODULE_3_PATH}
+cp -r "../../SHSsettings/shs_settings.h" "${MODULE_3_PATH}/SHScore"
 
 mkdir -p "${MODULE_3_PATH}/SHSlogos"
 cp -r "../../../SHSlogos/SVG/SHS_logo-mint.svg" "${MODULE_3_PATH}/SHSlogos"

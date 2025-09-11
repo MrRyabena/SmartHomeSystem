2d62eac6acddbbf3fe561c02ee3feb08bccaecaf
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 14:30:31 +0300

    Replase floor and ceil with lower and upper (bounds).

--- src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.cpp
+++ src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.cpp
@@ -12,21 +12,21 @@ shs::PhotoSensor::PhotoSensor(const shs::t::shs_pin_t pin, const uint8_t samples
 {}
 
 
-shs::PhotoSensor::PhotoSensor(const shs::t::shs_pin_t pin, const uint16_t floor_bound, const uint16_t ceil_bound, const uint8_t samples)
+shs::PhotoSensor::PhotoSensor(const shs::t::shs_pin_t pin, const uint16_t lower_bound, const uint16_t upper_bound, const uint8_t samples)
     :
     Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::PhotoSensor)),
     SensorAnalog(pin, samples),
-    m_floor_bound(floor_bound), m_ceil_bound(ceil_bound)
+    m_lower_bound(lower_bound), m_upper_bound(upper_bound)
 {}
 
 
-void shs::PhotoSensor::setBounds(uint16_t floor_bound, uint16_t ceil_bound)
+void shs::PhotoSensor::setBounds(uint16_t lower_bound, uint16_t upper_bound)
 {
-    if (floor_bound >= ceil_bound)
+    if (lower_bound >= upper_bound)
     {
-        m_floor_bound = m_ceil_bound = 0;
+        m_lower_bound = m_upper_bound = 0;
     }
 
-    m_floor_bound = floor_bound;
-    m_ceil_bound = ceil_bound;
+    m_lower_bound = lower_bound;
+    m_upper_bound = upper_bound;
 }
--- src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.h
+++ src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.h
@@ -18,14 +18,14 @@ public:
     enum class Status : uint8_t { OK, INPUT_ERROR };
 
     explicit PhotoSensor(shs::t::shs_pin_t pin, uint8_t samples = SENSOR_AVERAGE_SAMPLES);
-    explicit PhotoSensor(shs::t::shs_pin_t pin, uint16_t floor_bound, uint16_t ceil_bound, uint8_t samples = SENSOR_AVERAGE_SAMPLES);
+    explicit PhotoSensor(shs::t::shs_pin_t pin, uint16_t lower_bound, uint16_t upper_bound, uint8_t samples = SENSOR_AVERAGE_SAMPLES);
 
-    void setBounds(uint16_t floor_bound, uint16_t ceil_bound);
-    void setFloorBound(const uint16_t floor_bound) { setBounds(floor_bound, m_ceil_bound); }
-    void setCeilBound(const uint16_t ceil_bound) { setBounds(m_floor_bound, ceil_bound); }
+    void setBounds(uint16_t lower_bound, uint16_t upper_bound);
+    void setLowerBound(const uint16_t lower_bound) { setBounds(lower_bound, m_upper_bound); }
+    void setUpperBound(const uint16_t upper_bound) { setBounds(m_lower_bound, upper_bound); }
 
-    [[nodiscard]] uint16_t getFloorBound() const { return m_floor_bound; }
-    [[nodiscard]] uint16_t getCeilBound() const { return m_ceil_bound; }
+    [[nodiscard]] uint16_t getLowerBound() const { return m_lower_bound; }
+    [[nodiscard]] uint16_t getUpperBound() const { return m_upper_bound; }
 
     [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return m_getValue<int32_t>(metric); }
     [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return static_cast<shs::t::shs_fixed_t>(m_getValue<shs::t::shs_float_t>(metric)); }
@@ -34,19 +34,19 @@ public:
 
 
 protected:
-    uint16_t m_floor_bound{};
-    uint16_t m_ceil_bound{};
+    uint16_t m_lower_bound{};
+    uint16_t m_upper_bound{};
 
     template <typename T>
     T m_getValue(const uint8_t metric) const
     {
         if (m_metric == Metrics::RAW) return m_value;
-        if (m_floor_bound == m_ceil_bound) return {};
+        if (m_lower_bound == m_upper_bound) return {};
 
         switch (static_cast<Metrics>(metric))
         {
-            case Metrics::PERCENT: return map(static_cast<T>(m_value), m_floor_bound, m_ceil_bound, 0, 100); break;
-            case Metrics::NORMALIZED_255: return map(static_cast<T>(m_value), m_floor_bound, m_ceil_bound, 0, 255); break;
+            case Metrics::PERCENT: return map(static_cast<T>(m_value), m_lower_bound, m_upper_bound, 0, 100); break;
+            case Metrics::NORMALIZED_255: return map(static_cast<T>(m_value), m_lower_bound, m_upper_bound, 0, 255); break;
         }
 
         return {};

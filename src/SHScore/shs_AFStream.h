8affb7c290f2efab5198076bbc85c551a3ebe31c
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 10:38:03 +0300

    Starting development file stream class.

--- src/SHScore/shs_AFStream.h
+++ src/SHScore/shs_AFStream.h
@@ -6,3 +6,21 @@
 #include <ios>
 
 
+namespace shs
+{
+    template <class, class>
+    class Basic_AFStream;
+
+    using AFStream = Basic_AFStream<char, std::char_traits<char>>;
+}
+
+
+template <class CharT, class Traits = std::char_traits<ChartT>>
+class shs::Basic_AFStream : public basic_fstream
+{
+public:
+     
+
+protected:
+
+};

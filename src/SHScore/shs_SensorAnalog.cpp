5673e6680b1828eb090d0ffba1676ea9c5298880
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 14:10:24 +0300

    Separated into .h and .cpp files.

---
+++ src/SHScore/shs_SensorAnalog.cpp
@@ -0,0 +1,35 @@
+#include "shs_SensorAnalog.h"
+
+#include "shs_settings_private.h"
+
+#ifdef SHS_SF_ARDUINO
+#include <Arduino.h>
+#endif 
+
+#include "shs_Sensor.h"
+
+
+void shs::SensorAnalog::setup()
+{
+#ifdef SHS_SF_ARDUINO
+    pinMode(m_pin, INPUT);
+#endif
+}
+
+
+void shs::SensorAnalog::update()
+{
+#ifdef SHS_SF_ARDUINO
+    uint32_t average{};
+    for (auto i = 0u; i < m_samples; i++) average += analogRead(m_pin);
+    m_value = static_cast<shs::t::shs_float_t>(average) / m_samples;
+#endif
+}
+
+
+void shs::SensorAnalog::updateFast()
+{
+#ifdef SHS_SF_ARDUINO
+    m_value = analogRead(m_pin);
+#endif
+}

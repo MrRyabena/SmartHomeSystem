8df445a0569318a6971ef52d9f1a121c48d3920a
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 14:18:51 +0300

    Update constructors and includes.

--- src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.cpp
+++ src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.cpp
@@ -1,10 +1,22 @@
 #include "shs_lib_PhotoSensor.h"
 
+#include "shs_lib_SensorTypes.h"
+
 #include <shs_SensorAnalog.h>
 
 
 shs::PhotoSensor::PhotoSensor(const shs::t::shs_pin_t pin, const uint8_t samples)
-    : Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::PhotoSensor)), SensorAnalog(pin, samples)
+    :
+    Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::PhotoSensor)),
+    SensorAnalog(pin, samples)
+{}
+
+
+shs::PhotoSensor::PhotoSensor(const shs::t::shs_pin_t pin, const uint16_t floor_bound, const uint16_t ceil_bound, const uint8_t samples)
+    :
+    Sensor(static_cast<shs::Sensor::Type>(shs::lib::SensorTypes::PhotoSensor)),
+    SensorAnalog(pin, samples),
+    m_floor_bound(floor_bound), m_ceil_bound(ceil_bound)
 {}
 
 

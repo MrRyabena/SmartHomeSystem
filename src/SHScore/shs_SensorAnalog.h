8962577198a1a4e48a86971d4dc8f1a3a6e630cf
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 14:11:24 +0300

    Value type of protected member m_value replaced from shs::t::shs_fixed_t to shs::t::shs_float_t. The class is available for compilation without the flag SHS_SF_ARDUINO, but the functions are presented as stubs, without implementing the functionality.

--- src/SHScore/shs_SensorAnalog.h
+++ src/SHScore/shs_SensorAnalog.h
@@ -14,28 +14,31 @@
       - Support for multiple metrics.
     v2.1.0 — updated according to the changes in the base class.
     v2.2.0 — fixed a bug in function getCacheExpiration().
+    v2.3.0 — 
+      - Separated into .h and .cpp files.
+      - Value type of protected member m_value replaced from shs::t::shs_fixed_t 
+        to shs::t::shs_float_t.
+      - The class is available for compilation without the flag SHS_SF_ARDUINO, but the functions 
+        are presented as stubs, without implementing the functionality.
 */
 
 
-#include "shs_settings_private.h"
-
-#ifdef SHS_SF_ARDUINO
-
-
-#include <Arduino.h>
-
-#include "shs_Sensor.h"
 #include "shs_types.h"
 
 
 namespace shs
 {
+    class Sensor;          // declared in shs_Sensor.h
     class SensorAnalog;
 }
 
 
-/*
-  A class for getting values from analog pins to which sensors are connected.
+/**
+ * @brief
+ * A class for getting values from analog pins to which sensors are connected.
+ * 
+ * @note The class is available for compilation without the flag SHS_SF_ARDUINO, but the functions 
+ *       are presented as stubs, without implementing the functionality.
 */
 class shs::SensorAnalog : public shs::Sensor
 {
@@ -47,36 +50,27 @@ public:
     static constexpr uint8_t SENSOR_AVERAGE_SAMPLES = SHS_SET_SENSOR_AVERAGE_SAMPLES;
 
     void setSamples(const uint8_t samples) { m_samples = samples; }
-    uint8_t getSamples() const { return m_samples; }
+    [[nodiscard]] uint8_t getSamples() const { return m_samples; }
 
-    void setup() override { pinMode(m_pin, INPUT); }
+    void setup() override;
 
-    void update() override
-    {
-        uint32_t average{};
-        for (auto i = 0u; i < m_samples; i++) average += analogRead(m_pin);
-        m_value = shs::t::shs_float_t(average) / m_samples;
-    }
-
-    void updateFast() override { m_value = analogRead(m_pin); }
+    void update() override;
+    void updateFast() override;
     void updateForced(bool fast = false) override { fast ? updateFast() : update(); }
     [[nodiscard]] bool isUpdated() override { return true; }
     [[nodiscard]] uint8_t getStatus() override { return 0; }
 
     void clearCache() override {}
     void setCacheExpiration(const uint32_t expiration_time) override {}
-    uint32_t getCacheExpiration() const override { return 0; }
+    [[nodiscard]] uint32_t getCacheExpiration() const override { return 0; }
 
-    [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return m_value; }
-    [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return m_value; }
-    [[nodiscard]] shs::t::shs_float_t  getValueF(const uint8_t metric = 0) override { return static_cast<shs::t::shs_float_t>(m_value); }
+    [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return static_cast<int32_t>(m_value); }
+    [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return static_cast<shs::t::shs_fixed_t>(m_value); }
+    [[nodiscard]] shs::t::shs_float_t  getValueF(const uint8_t metric = 0) override { return m_value; }
     [[nodiscard]] shs::t::shs_double_t getValueD(const uint8_t metric = 0) override { return static_cast<shs::t::shs_double_t>(m_value); }
 
 protected:
-    shs::t::shs_fixed_t m_value;
+    shs::t::shs_float_t m_value;
     const uint8_t m_pin;
     uint8_t m_samples;
 };
-
-
-#endif  // #ifdef SHS_SF_ARDUINO

88d7b5410b1e7998157f92fb8576dc4d3a47f945
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-09-11 14:18:22 +0300

    Added new constructor, const and [[nodiscard]] in functions.

--- src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.h
+++ src/SHSlibrary/SHS_Photo/shs_lib_PhotoSensor.h
@@ -3,9 +3,6 @@
 
 #include <shs_types.h>
 
-#include "shs_lib_SensorTypes.h"
-#include "shs_settings_private.h"
-
 
 namespace shs
 {
@@ -20,14 +17,15 @@ public:
     enum class Metrics : uint8_t { RAW, PERCENT, NORMALIZED_255 };
     enum class Status : uint8_t { OK, INPUT_ERROR };
 
-    explicit PhotoSensor(const shs::t::shs_pin_t pin, const uint8_t samples = SENSOR_AVERAGE_SAMPLES);
+    explicit PhotoSensor(shs::t::shs_pin_t pin, uint8_t samples = SENSOR_AVERAGE_SAMPLES);
+    explicit PhotoSensor(shs::t::shs_pin_t pin, uint16_t floor_bound, uint16_t ceil_bound, uint8_t samples = SENSOR_AVERAGE_SAMPLES);
 
     void setBounds(uint16_t floor_bound, uint16_t ceil_bound);
     void setFloorBound(const uint16_t floor_bound) { setBounds(floor_bound, m_ceil_bound); }
     void setCeilBound(const uint16_t ceil_bound) { setBounds(m_floor_bound, ceil_bound); }
 
-    uint16_t getFloorBound() const { return m_floor_bound; }
-    uint16_t getCeilBound() const { return m_ceil_bound; }
+    [[nodiscard]] uint16_t getFloorBound() const { return m_floor_bound; }
+    [[nodiscard]] uint16_t getCeilBound() const { return m_ceil_bound; }
 
     [[nodiscard]] int32_t              getValueI(const uint8_t metric = 0) override { return m_getValue<int32_t>(metric); }
     [[nodiscard]] shs::t::shs_fixed_t  getValueFx(const uint8_t metric = 0) override { return static_cast<shs::t::shs_fixed_t>(m_getValue<shs::t::shs_float_t>(metric)); }
@@ -40,7 +38,7 @@ protected:
     uint16_t m_ceil_bound{};
 
     template <typename T>
-    T m_getValue(const uint8_t metric)
+    T m_getValue(const uint8_t metric) const
     {
         if (m_metric == Metrics::RAW) return m_value;
         if (m_floor_bound == m_ceil_bound) return {};

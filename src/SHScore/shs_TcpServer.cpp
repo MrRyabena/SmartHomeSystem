f16facbead36cc10613d3871416f206ed927da47
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-09-20 02:00:49 +0300

    Debbugging.

--- src/SHScore/shs_TcpServer.cpp
+++ src/SHScore/shs_TcpServer.cpp
@@ -2,45 +2,65 @@
 
 #if defined(SHS_SF_NETWORK) && defined(SHS_SF_ARDUINO)
 
+#define SHS_SF_DEBUG
+#include "shs_debug.h"
 void shs::TcpServer::tick()
 {
     if (m_connecting_client && m_connecting_client->client.connected())
     {
+        doutln("client connected");
         if (shs::ProgramTime::s_milliseconds() - m_connecting_client_time > max_connection_time)
         {
+            doutln("max_connection_time exceeded");
             m_connecting_client->stop();
             return;
         }
 
 
-        if (m_connecting_client->checkBus() != shs::DTPbus::packet_received && m_connecting_client->status != shs::DTPbus::packet_processed) return;
-
-
-        if (shs::DTPpacket::get_DTPcode(m_connecting_client->getLastData()) == shs::DTPpacket::DTPcode::INITIAL_ANSWER || shs::DTPpacket::get_DTPcode(m_connecting_client->getLastData()) == shs::DTPpacket::DTPcode::INITIAL)
+        if (m_connecting_client->checkBus() != shs::DTPbus::packet_received && m_connecting_client->status != shs::DTPbus::packet_processed) 
         {
-            auto answer = shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, true);
-
-            m_connecting_client->sendPacket(answer);
-
-            m_dtp.attachBus(std::move(m_connecting_client));
+            doutln("waiting for INITIAL_ANSWER or INITIAL packet");
+            return;
         }
         else
         {
-            m_connecting_client->sendPacket(shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, false));
-            m_connecting_client->stop();
+            doutln("data received from client");
         }
+
+        // if (shs::DTPpacket::get_DTPcode(m_connecting_client->getLastData()) == shs::DTPpacket::DTPcode::INITIAL_ANSWER || shs::DTPpacket::get_DTPcode(m_connecting_client->getLastData()) == shs::DTPpacket::DTPcode::INITIAL)
+        // {
+            //doutln("INITIAL_ANSWER received");
+            auto answer = shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, true);
+
+            m_connecting_client->sendPacket(answer);
+    
+            m_dtp.attachBus(std::move(m_connecting_client));
+            doutln("client attached to DTP");
+        // }
+        // else
+        // {
+        //     doutln("not INITIAL_ANSWER or INITIAL packet, disconnecting client");
+        //     auto it = m_connecting_client->getLastData(); 
+        //     for (auto i = 0; i < it.size(); i++) doutln(int(it[i]));
+        //     m_connecting_client->sendPacket(shs::DTP_APIpackets::getInitialAnswerPacket(m_dtp.moduleID, false));
+        //     m_connecting_client->stop();
+        // }
     }
 
     if (server.hasClient())
     {
+        doutln("new client connecting");
         auto client = server.available();
         if (!client) return;
-        m_connecting_client = std::make_unique<shs::TcpSocket>(client, m_dtp.getUniqueBusID(), nullptr, 25, nullptr, [this](shs::TcpSocket& socket) { socket.setActive(false); });
+        m_connecting_client = std::make_unique<shs::TcpSocket>(client, m_dtp.getUniqueBusID(), nullptr, 25, nullptr, [this](shs::TcpSocket& socket) { socket.setActive(false); doutln("client unactive"); });
 
         m_connecting_client_time = shs::ProgramTime::s_milliseconds();
 
+        doutln("sending INITIAL packet");
         auto mes = shs::DTP_APIpackets::getInitialPacket(m_dtp.moduleID);
+        doutln("sending packet:");
         m_connecting_client->sendPacket(mes);
+        doutln("packet sent");
     }
 }
 

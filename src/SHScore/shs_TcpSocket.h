c6ff2c2cd56097e2c8cc3e8e6475595e97f0a1b6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-09-21 12:39:25 +0300

    Redesigned, in testing.

--- src/SHScore/shs_TcpSocket.h
+++ src/SHScore/shs_TcpSocket.h
@@ -12,7 +12,6 @@
     v2.1.0 — fixed bugs related to shs::DTP_API.
 */
 
-
 #include <stdint.h>
 #include <functional>
 
@@ -21,7 +20,7 @@
 #ifdef SHS_SF_ARDUINO
 #include <Arduino.h>
 
-#ifdef SHS_SF_ESP8266 
+#ifdef SHS_SF_ESP8266
 #include <ESP8266WiFi.h>
 #else
 #include <WiFi.h>
@@ -32,7 +31,6 @@
 #include <QObject>
 #endif
 
-
 #include "shs_Process.h"
 #include "shs_types.h"
 #include "shs_DTP_API.h"
@@ -40,128 +38,96 @@
 #include "shs_types.h"
 #include "shs_API.h"
 #include "shs_ProgramTime.h"
-
+#include "shs_ProgramTimer.h"
 
 namespace shs
 {
     class TcpSocket;
 };
 
-
 class shs::TcpSocket : public shs::DTPbus
 {
 public:
-
 #ifdef SHS_SF_ARDUINO
     WiFiClient client;
 #elif defined(SHS_SF_QT)
     shs::qt::TcpSocket client;
 #endif
 
+    enum Status : uint8_t
+    {
+        DISCONNECTED,
+        CONNECTING,
+        RECONNECTING,
+        CONNECTED,
+        INACTIVE
+    };
 
-#define default_connect_callback ([](shs::TcpSocket& socket) \
-        { if (socket.isActive()) socket.sendPacket(shs::DTP_APIpackets::getInitialPacket(socket.busID)); })
+    static std::function<void(shs::TcpSocket&)> default_connect_callback;
+    static std::function<void(shs::TcpSocket&)> default_disconnect_callback;
 
-#define default_disconnect_callback ([](shs::TcpSocket& socket) {  if (socket.isActive()) socket.reconnect(); })
-
-
-    explicit TcpSocket
-    (
-        const shs::t::shs_IP_t& hostIP, const shs::t::shs_port_t port,
-        const shs::t::shs_busID_t busID, shs::API* handler = nullptr, const uint8_t bufsize = 25,
+    explicit TcpSocket(
+        const shs::t::shs_IP_t& hostIP, shs::t::shs_port_t port,
+        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = 25,
         const std::function<void(shs::TcpSocket&)>& connect_callback = default_connect_callback,
-        const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback
-    )
-        :
-        DTPbus(busID, handler, bufsize),
-        m_hostIP(hostIP), m_port(port), m_active_flag(true),
-        m_connect_callback(connect_callback), m_disconnect_callback(disconnect_callback)
-    {}
+        const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback);
 
-
-    explicit TcpSocket
-    (
+    explicit TcpSocket(
     #ifdef SHS_SF_ARDUINO
         const WiFiClient& parent,
     #elif defined(SHS_SF_QT)
         QObject* parent,
         const shs::t::shs_IP_t& hostIP, const shs::t::shs_port_t port,
     #endif
-        const shs::t::shs_busID_t busID, shs::API* handler = nullptr, const uint8_t bufsize = 25,
+        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = 25,
         const std::function<void(shs::TcpSocket&)>& connect_callback = default_connect_callback,
-        const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback
-    )
-        :
-        DTPbus(busID, handler, bufsize),
-    #ifdef SHS_SF_QT
-        m_hostIP(hostIP), m_port(port),
-    #endif
-        client(parent), m_active_flag(true),
-        m_connect_callback(connect_callback), m_disconnect_callback(disconnect_callback)
-    {}
+        const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback);
 
     ~TcpSocket() = default;
 
-
     // -------------------- native functions -----------------------------------
 
-    void reconnect() { m_connect(); }
+    void connect(shs::t::shs_time_t connecting_timeout = 0);
+    void reconnect(shs::t::shs_time_t reconnecting_timeout = 0);
+
+    void setConnectingTimeout(const shs::t::shs_time_t timeout) { m_connecting_timeout = timeout; }
+    void setReconnectingTimeout(const shs::t::shs_time_t timeout) { m_reconnecting_timeout = timeout; }
+    shs::t::shs_time_t getConnectingTimeout() const { return m_connecting_timeout; }
+    shs::t::shs_time_t getReconnectingTimeout() const { return m_reconnecting_timeout; }
 
     shs::t::shs_IP_t getIP() const { return m_hostIP; }
     shs::t::shs_port_t getPort() const { return m_port; }
 
-    void setActive(const bool flag) { m_active_flag = flag; }
-
+    void setActive(const bool flag);
     bool connected() { return client.connected(); }
-
+    shs::TcpSocket::Status getStatus() const { return m_status; }
 
     // -------------------- shs::Process (from shs::DTPbus) --------------------
-    void start() override { m_connect(); }
-    void tick() override { if (!client.connected() && m_disconnect_callback) m_disconnect_callback(*this); }
-    void stop() override { if (client.connected()) client.stop(); }
-
+    void start() override;
+    void tick() override;
+    void stop() override { if (client.connected()) client.stop(); m_status = Status::DISCONNECTED; }
 
     // -------------------- shs::DTPbus ----------------------------------------
-    bool isActive() const override { return m_active_flag; }
+    bool isActive() const override { return m_status != Status::INACTIVE; }
 
-    Status checkBus() override { return shs::DTPbus::checkBus(client); }
+    shs::DTPbus::Status checkBus() override { return shs::DTPbus::checkBus(client); }
 
     uint8_t sendPacket(const shs::DTPpacket& packet) override { return shs::DTPbus::sendPacket(client, packet); }
     uint8_t sendRAW(shs::ByteCollector<>& bc) override { return shs::DTPbus::sendRAW(client, bc); }
     uint8_t sendRAW(shs::ByteCollectorReadIterator<>& it) override { return shs::DTPbus::sendRAW(client, it); }
     uint8_t sendRAW(const uint8_t* data, const uint8_t size) override { return shs::DTPbus::sendRAW(client, data, size); }
 
-
 private:
     shs::t::shs_IP_t m_hostIP;
     shs::t::shs_port_t m_port{};
-    bool m_active_flag;
+    shs::TcpSocket::Status m_status;
+
+    shs::ProgramTime m_timer;
+
+    shs::t::shs_time_t m_connecting_timeout = 2000;
+    shs::t::shs_time_t m_reconnecting_timeout = 5000;
 
     std::function<void(shs::TcpSocket& client)> m_connect_callback;
     std::function<void(shs::TcpSocket& client)> m_disconnect_callback;
 
-
-    void m_connect()
-    {
-        if (!m_active_flag) return;
-        
-        bool connected = false;
-        #ifdef SHS_SF_ARDUINO
-            connected = client.connect(m_hostIP, m_port);
-        #elif defined(SHS_SF_QT)
-            connected = client.connectToHost(m_hostIP, m_port);
-        #endif
-        
-        if (connected) //&& m_connect_callback) {
-          {
-            sendPacket(shs::DTP_APIpackets::getInitialPacket(busID)); 
-            //shs::DTPpacket packet(busID, 0, shs::ByteCollector<>(10));
-           // sendPacket(packet);
-           // try {
-                //m_connect_callback(*this);
-            ///} catch (...) {
-                
-            //}
-        }
-    }
 };

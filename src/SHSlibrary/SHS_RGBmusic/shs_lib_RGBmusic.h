4fa3ada1896f16944a9ca51f6d716bb4abff6e3e
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2026-02-18 12:48:48 +0300

    Updated, fixed bugs, tested.

--- src/SHSlibrary/SHS_RGBmusic/shs_lib_RGBmusic.h
+++ src/SHSlibrary/SHS_RGBmusic/shs_lib_RGBmusic.h
@@ -6,11 +6,13 @@
     https://github.com/AlexGyver/ColorMusicRGB
 */
 
+#include <Arduino.h>
 
 #include <VolAnalyzer.h>  // https://github.com/GyverLibs/VolAnalyzer
 #include <GRGB.h>         // https://github.com/GyverLibs/GRGB
 
 #include <shs_Process.h>
+#include <shs_debug.h>
 
 
 namespace shs
@@ -18,45 +20,64 @@ namespace shs
     class RGBmusic;
 }
 
-
 class shs::RGBmusic : public shs::Process
 {
 public:
-    enum Effects : uint8_t { OFF, SHIFT_151, SHIFT_129, PULSE_WHEEL, FIRE }
-    effect;
+    enum Effects : uint8_t { OFF, SHIFT_151, SHIFT_129, PULSE_WHEEL, FIRE };
 
     VolAnalyzer analyzer;
     GRGB& grgb;
 
-    RGBmusic(GRGB& set_grgb, const int8_t port = -1) : grgb(set_grgb), analyzer(port) {}
+    RGBmusic(GRGB& set_grgb, const int8_t port = -1, uint8_t min_bright = 0)
+        : grgb(set_grgb), analyzer(port), m_min_bright(min_bright)
+    {}
 
-    void setEffect(const Effects eff) { effect = eff; }
+    void setEffect(const Effects eff) { doutln("set music effect"); m_effect = eff; if (m_effect == OFF) stop(); }
+    Effects getEffect() const { return m_effect; }
 
-    void start() override { grgb.setFade(false); }
+    void setMinBright(const uint8_t br) { m_min_bright = br; }
+    uint8_t getMinBright() const { return m_min_bright; }
+
+    void start() override { grgb.fadeMode(false); }
 
     void tick() override
     {
-        switch (effect)
+        if (m_effect == OFF) return;
+        if (!analyzer.tick()) return;
+
+        // Serial.print(analyzer.pulse() * 20); // скачок громкости
+        // Serial.print(',');
+        // Serial.print(analyzer.getVol());    // громкость 0-100
+        // Serial.print(',');
+        // Serial.print(analyzer.getRaw());    // сырая величина
+        // Serial.print(',');
+        // Serial.println(analyzer.getMax());  // амплитудная огибающая
+
+        switch (m_effect)
         {
-            case OFF: break;
             case SHIFT_151:
-                if (analyzer.getPulse()) color += 151;
-                led.setWheel8(color, analyzer.getVol());
+                if (analyzer.pulse()) m_color += 151;
+                grgb.setWheel8(m_color, m_min_bright + analyzer.getVol() >= 255 ? 255 : m_min_bright + analyzer.getVol());
                 break;
             case SHIFT_129:
-                if (analyzer.getPulse()) color += 129;
-                led.setWheel8(color, analyzer.getVol());
+                if (analyzer.getPulse()) m_color += 129;
+                grgb.setWheel8(m_color, m_min_bright + analyzer.getVol() >= 255 ? 255 : m_min_bright + analyzer.getVol());
                 break;
             case PULSE_WHEEL:
-                led.setWheel8(color++, analyzer.getVol());
+                grgb.setWheel8(m_color++, m_min_bright + analyzer.getVol() >= 255 ? 255 : m_min_bright + analyzer.getVol());
                 break;
             case FIRE:
-                led.setHSVfast((analyzer.getVol() - 10) / 6, 255, analyzer.getVol());
+                grgb.setHSVfast((analyzer.getVol() - 10) / 6, 255, m_min_bright + analyzer.getVol() >= 255 ? 255 : m_min_bright + analyzer.getVol());
                 break;
             default: break;
         }
     }
 
-    void stop() override { mode = OFF; grgb.setRGB(0, 0, 0); }
+    void stop() override { m_effect = OFF; grgb.setRGB(0, 0, 0); }
+
+protected:
+    uint8_t m_color{};
+    uint8_t m_min_bright{};
+    Effects m_effect{};
 
 };

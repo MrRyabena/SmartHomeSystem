db42ecb66ca80a17b1262b92ab376c14520d2cd1
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-05-09 23:45:15 +0300

    Added docs.

--- src/SHScore/shs_MutexSafe.h
+++ src/SHScore/shs_MutexSafe.h
@@ -6,10 +6,17 @@ namespace shs
     class MutexSafe;
 }
 
+/**
+ * @brief Scope guard that locks a mutex on construction and unlocks it on destruction.
+ * @tparam Mutex_t Mutex type used by the current platform.
+ */
 template <typename Mutex_t>
 class shs::MutexSafe
 {
 public:
+    /**
+     * @brief Locks the given mutex for the lifetime of this guard.
+     */
     explicit MutexSafe(Mutex_t &mutex) : m_mutex(mutex)
     {
 #ifdef SHS_SF_FreeRTOS
@@ -19,6 +26,9 @@ public:
 #endif
     }
 
+    /**
+     * @brief Unlocks the guarded mutex.
+     */
     ~MutexSafe()
     {
 #ifdef SHS_SF_FreeRTOS
@@ -28,10 +38,24 @@ public:
 #endif
     }
 
+    /**
+     * @brief Copying is disabled for mutex guards.
+     */
     MutexSafe(const MutexSafe &) = delete;
+
+    /**
+     * @brief Copy assignment is disabled for mutex guards.
+     */
     MutexSafe &operator=(const MutexSafe &) = delete;
 
+    /**
+     * @brief Transfers guard ownership without unlocking the mutex.
+     */
     MutexSafe(MutexSafe &&other) noexcept : m_mutex(other.m_mutex) { other.m_mutex = nullptr; }
+
+    /**
+     * @brief Move assignment is disabled for mutex guards.
+     */
     MutexSafe &operator=(MutexSafe &&other) = delete;
 
 private:

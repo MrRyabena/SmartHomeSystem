7e9988d75eba5b442a8dd2c4439c1519105c06b6
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-08-31 02:36:16 +0300

    Replace bufsize constant with DTPbusReceiveContext::DEFAULT_BUFFER_SIZE

--- src/SHScore/shs_TcpSocket.h
+++ src/SHScore/shs_TcpSocket.h
@@ -39,6 +39,7 @@
 #include "shs_types.h"
 #include "shs_DTP_API.h"
 #include "shs_DTPbus.h"
+#include "shs_DTPbusReceiveContext.h"
 #include "shs_types.h"
 #include "shs_API.h"
 #include "shs_ProgramTime.h"
@@ -72,7 +73,7 @@ public:
 
     explicit TcpSocket(
         const shs::t::shs_IP_t& hostIP, shs::t::shs_port_t port,
-        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = 25,
+        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = shs::DTPbusReceiveContext::DEFAULT_BUFFER_SIZE,
         const std::function<void(shs::TcpSocket&)>& connect_callback = default_connect_callback,
         const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback);
 
@@ -83,7 +84,7 @@ public:
         QObject* parent,
         const shs::t::shs_IP_t& hostIP, const shs::t::shs_port_t port,
     #endif
-        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = 25,
+        shs::t::shs_busID_t busID, shs::API* handler = nullptr, uint8_t bufsize = shs::DTPbusReceiveContext::DEFAULT_BUFFER_SIZE,
         const std::function<void(shs::TcpSocket&)>& connect_callback = default_connect_callback,
         const std::function<void(shs::TcpSocket&)>& disconnect_callback = default_disconnect_callback);
 

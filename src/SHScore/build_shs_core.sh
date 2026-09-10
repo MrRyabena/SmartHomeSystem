09b853b0bcf33e886ae09b7179e67df915c02dbd
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2026-09-11 01:44:44 +0300

    Correct build script

--- src/SHScore/build_shs_core.sh
+++ src/SHScore/build_shs_core.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
 mkdir -p build
 cd build
 cmake -DSHS_CMF_QT=OFF -DSHS_CMF_UNUSE_SHS_SETTINGS=ON -DCMAKE_CXX_FLAGS="-Wall" ..
